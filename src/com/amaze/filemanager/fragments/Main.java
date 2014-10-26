22caf0b930cdf8d12906959bcced755ab55ade90
arpit khurana <arpitkh96@gmail.com>
2014-10-26 07:49:19 +0530

    Revert "Added header"
    This reverts commit 6bda4e67ed29d6a2cb9850aaa8aa29e813f8b3e9.

--- src/com/amaze/filemanager/fragments/Main.java
+++ src/com/amaze/filemanager/fragments/Main.java
@@ -142,8 +142,6 @@ public class Main extends android.support.v4.app.Fragment {
     private FloatingActionButton fab;
     private TabSpinnerAdapter tabSpinnerAdapter;
     public float[] color;
-    View header;
-    ImageView headerbackbutton;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -292,15 +290,7 @@ public class Main extends android.support.v4.app.Fragment {
         footerView=getActivity().getLayoutInflater().inflate(R.layout.divider,null);
 
         if (aBoolean) {
-            header=getActivity().getLayoutInflater().inflate(R.layout.listheader, null);
-            header.findViewById(R.id.headerback).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    goBack();
-                }
-            });
-            headerbackbutton=(ImageView)header.findViewById(R.id.headerbackbuton);
-            listView.addHeaderView(header);
+
             listView.addFooterView(footerView);
             listView.setFastScrollEnabled(true);
         } else {
@@ -636,8 +626,6 @@ if(listView!=null){
 
                     results = false;
                     current = f.getPath();
-                    if(current.equals("/")){headerbackbutton.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,0));}
-                    else {headerbackbutton.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT));}
                     if (back) {
                         if (scrolls.containsKey(current)) {
                             Bundle b = scrolls.get(current);
