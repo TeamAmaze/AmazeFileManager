547368d87c15efcc28f21ef823bc3a13a052c440
arpit khurana <arpitkh96@gmail.com>
2015-04-22 23:20:54 +0530

    Added dividers

--- src/com/amaze/filemanager/fragments/Main.java
+++ src/com/amaze/filemanager/fragments/Main.java
@@ -37,10 +37,13 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Rect;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.media.RingtoneManager;
 import android.net.Uri;
@@ -55,6 +58,7 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.ActionMode;
 import android.view.Gravity;
@@ -155,6 +159,7 @@ public class Main extends android.support.v4.app.Fragment {
     SwipeRefreshLayout mSwipeRefreshLayout;
     boolean addheader=true;
     StickyRecyclerHeadersDecoration headersDecor;
+    DividerItemDecoration dividerItemDecoration;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -255,7 +260,8 @@ public class Main extends android.support.v4.app.Fragment {
             listView.setLayoutManager(mLayoutManager);
         } else {
             listView.setLayoutManager(mLayoutManagerGrid);
-        }return rootView;
+        }
+        return rootView;
     }
 
     @Override
@@ -517,8 +523,10 @@ public class Main extends android.support.v4.app.Fragment {
 
                         listView.setAdapter(adapter);
 
-                    if(!addheader && aBoolean){listView.removeItemDecoration(headersDecor);addheader=true;}
+                    if(!addheader && aBoolean){listView.removeItemDecoration(dividerItemDecoration);listView.removeItemDecoration(headersDecor);addheader=true;}
                     if(addheader && aBoolean){
+                        dividerItemDecoration=new DividerItemDecoration(getActivity(),DividerItemDecoration.VERTICAL_LIST);
+                        listView.addItemDecoration(dividerItemDecoration);
                      headersDecor = new StickyRecyclerHeadersDecoration(adapter);
                     listView.addItemDecoration(headersDecor);addheader=false;}
                     results = false;
@@ -1510,6 +1518,86 @@ public class Main extends android.support.v4.app.Fragment {
             }
         });
 
+    }
+}
+ class DividerItemDecoration extends RecyclerView.ItemDecoration {
+
+    private static final int[] ATTRS = new int[]{
+            android.R.attr.listDivider
+    };
+
+    public static final int HORIZONTAL_LIST = LinearLayoutManager.HORIZONTAL;
+
+    public static final int VERTICAL_LIST = LinearLayoutManager.VERTICAL;
+
+    private Drawable mDivider;
+
+    private int mOrientation;
+    int pix=0,leftpix=0;
+    public DividerItemDecoration(Context context, int orientation) {
+        final TypedArray a = context.obtainStyledAttributes(ATTRS);
+        mDivider = a.getDrawable(0);
+        a.recycle();
+        setOrientation(orientation);
+        pix=(int)(16*(context.getResources().getDisplayMetrics().densityDpi/160f));
+        leftpix=(int)(72*(context.getResources().getDisplayMetrics().densityDpi/160f));
+    }
+
+    public void setOrientation(int orientation) {
+        if (orientation != HORIZONTAL_LIST && orientation != VERTICAL_LIST) {
+            throw new IllegalArgumentException("invalid orientation");
+        }
+        mOrientation = orientation;
+    }
+
+    @Override
+    public void onDraw(Canvas c, RecyclerView parent) {
+        if (mOrientation == VERTICAL_LIST) {
+            drawVertical(c, parent);
+        } else {
+            drawHorizontal(c, parent);
+        }
+    }
+
+    public void drawVertical(Canvas c, RecyclerView parent) {
+        final int left = parent.getPaddingLeft()+leftpix;
+        final int right = parent.getWidth() - parent.getPaddingRight()-pix;
+
+        final int childCount = parent.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            final View child = parent.getChildAt(i);
+            final RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child
+                    .getLayoutParams();
+            final int top = child.getBottom() + params.bottomMargin;
+            final int bottom = top + mDivider.getIntrinsicHeight();
+            mDivider.setBounds(left, top, right, bottom);
+            mDivider.draw(c);
+        }
+    }
+
+    public void drawHorizontal(Canvas c, RecyclerView parent) {
+        final int top = parent.getPaddingTop();
+        final int bottom = parent.getHeight() - parent.getPaddingBottom();
+
+        final int childCount = parent.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            final View child = parent.getChildAt(i);
+            final RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child
+                    .getLayoutParams();
+            final int left = child.getRight() + params.rightMargin;
+            final int right = left + mDivider.getIntrinsicHeight();
+            mDivider.setBounds(left, top, right, bottom);
+            mDivider.draw(c);
+        }
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, int itemPosition, RecyclerView parent) {
+        if (mOrientation == VERTICAL_LIST) {
+            outRect.set(0, 0, 0, mDivider.getIntrinsicHeight());
+        } else {
+            outRect.set(0, 0, mDivider.getIntrinsicWidth(), 0);
+        }
     }
 }
 class SpacesItemDecoration extends RecyclerView.ItemDecoration {
