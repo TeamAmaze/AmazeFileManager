b270bfd9b80e12181eb93cb008ff394db0d10dfd
Vishal <vishalmeham2@gmail.com>
2017-01-10 01:23:41 +0530

    SRV: Implement serialization of service startup for multiple instances

--- src/main/java/com/amaze/filemanager/services/CopyService.java
+++ src/main/java/com/amaze/filemanager/services/CopyService.java
@@ -109,7 +109,7 @@ public class CopyService extends Service {
         mBuilder.setContentTitle(getResources().getString(R.string.copying))
                 .setSmallIcon(R.drawable.ic_content_copy_white_36dp);
         if(foreground){
-            startForeground(Integer.parseInt("456"+startId),mBuilder.build());
+            startForeground(Integer.parseInt("456"+startId), mBuilder.build());
             foreground=false;
         }
         b.putBoolean(TAG_COPY_MOVE, intent.getBooleanExtra(TAG_COPY_MOVE, false));
@@ -207,7 +207,7 @@ public class CopyService extends Service {
              */
             public int checkFolder(final String f,Context context) {
                 if(f==null)return 0;
-                if(f.startsWith("smb://") || f.startsWith("otg:"))return 1;
+                if(f.startsWith("smb://") || f.startsWith("otg:")) return 1;
                 File folder=new File(f);
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && FileUtil.isOnExtSdCard(folder, context)) {
                     if (!folder.exists() || !folder.isDirectory()) {
--- src/main/java/com/amaze/filemanager/utils/ServiceWatcherUtil.java
+++ src/main/java/com/amaze/filemanager/utils/ServiceWatcherUtil.java
@@ -2,11 +2,11 @@ package com.amaze.filemanager.utils;
 
 /**
  * Created by vishal on 4/1/17.
- * Inner class that watches over the copy progress and publish progress after certain
- * interval. We wouldn't want to interrupt copy loops by writing too much callbacks, and thus
- * possibly delaying the copy process.
+ *
+ * Helper class providing helper methods to manage Service startup and it's progress
+ * Be advised - this class can only handle progress with one object at a time. Hence, class also provides
+ * convenience methods to serialize the service startup.
  */
-
 
 import android.app.NotificationManager;
 import android.content.Context;
@@ -19,12 +19,16 @@ import com.amaze.filemanager.R;
 import com.amaze.filemanager.activities.BaseActivity;
 import com.amaze.filemanager.services.ProgressHandler;
 
+import java.util.ArrayList;
+
 public class ServiceWatcherUtil {
 
-    Handler handler;
-    HandlerThread handlerThread;
-    ProgressHandler progressHandler;
+    private Handler handler;
+    private static HandlerThread handlerThread;
+    private ProgressHandler progressHandler;
     long totalSize;
+
+    private static ArrayList<Intent> pendingIntents = new ArrayList<>();
 
     // position of byte in total byte size to be copied
     public static long POSITION = 0L;
@@ -39,7 +43,7 @@ public class ServiceWatcherUtil {
         this.totalSize = totalSize;
         POSITION = 0l;
 
-        handlerThread = new HandlerThread("copy_watcher");
+        handlerThread = new HandlerThread("service_progress_watcher");
         handlerThread.start();
         handler = new Handler(handlerThread.getLooper());
     }
@@ -73,47 +77,76 @@ public class ServiceWatcherUtil {
     }
 
     /**
-     * Convenient method to check whether another service is working in background
+     * Convenience method to check whether another service is working in background
      * If a service is found working (by checking and maintaining state of {@link BaseActivity#IS_BOUND}
      * which is further bound to service using {@link android.content.ServiceConnection} for it's state)
-     * then we wait for an interval of 5 secs, before checking on it again
+     * then we wait for an interval of 5 secs, before checking on it again.
+     * We're not depending on {@link BaseActivity#IS_BOUND} much as it becomes inconsistent after we
+     * close the app, as it is bound to lifecycle of {@link android.app.Activity}.
+     * {@see ServiceWatcherUtil#init()}
+     *
+     * Be advised - this method is not sure to start a new service, especially when app has been closed
+     * as there are higher chances for android system to GC the thread when it is running low on memory
      *
      * @param context
      * @param intent
      */
     public static void runService(final Context context, final Intent intent) {
 
-        if (!BaseActivity.IS_BOUND) {
+        if (!BaseActivity.IS_BOUND || handlerThread==null || !handlerThread.isAlive()) {
             // we're not bound, no need to proceed further and waste up resources
+            // start the service directly
             context.startService(intent);
             return;
         }
 
-        final HandlerThread handlerThread = new HandlerThread("service_watcher");
-        handlerThread.start();
-        final Handler handler = new Handler(handlerThread.getLooper());
+        if (pendingIntents.size()==0) {
+            init(context);
+        }
+        pendingIntents.add(intent);
+
+    }
+
+    /**
+     * Helper method to {@link #runService(Context, Intent)}
+     * Starts the wait watcher thread if not already started.
+     * Halting condition depends on various things - a boolean {@link BaseActivity#IS_BOUND} and
+     * {@link #handlerThread}, in case the boolean becomes inconsistent when Activity is closed (since
+     * Service is bound to the activity).
+     * @param context
+     */
+    private static void init(final Context context) {
+
+        final HandlerThread waitingThread = new HandlerThread("service_startup_watcher");
+        waitingThread.start();
+        final Handler handler = new Handler(waitingThread.getLooper());
         final NotificationManager notificationManager = (NotificationManager)
                 context.getSystemService(Context.NOTIFICATION_SERVICE);
-        NotificationCompat.Builder mBuilder=new NotificationCompat.Builder(context);
+        final NotificationCompat.Builder mBuilder=new NotificationCompat.Builder(context);
         mBuilder.setContentTitle(context.getString(R.string.waiting_title));
         mBuilder.setContentText(context.getString(R.string.waiting_content));
         mBuilder.setAutoCancel(false);
         mBuilder.setSmallIcon(R.drawable.ic_content_copy_white_36dp);
         mBuilder.setProgress(0, 0, true);
-        notificationManager.notify(1, mBuilder.build());
+        notificationManager.notify(9248, mBuilder.build());
 
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
-                if (!BaseActivity.IS_BOUND) {
+                if (!BaseActivity.IS_BOUND || handlerThread==null || !handlerThread.isAlive()) {
 
                     // service is been finished, let's start this one
-                    // and free up resources before returning from here
-                    context.startService(intent);
-                    notificationManager.cancel(1);
-                    handler.removeCallbacks(this);
-                    handlerThread.quit();
-                    return;
+
+                    // pop recent intent from pendingIntents
+                    context.startService(pendingIntents.remove(pendingIntents.size()-1));
+
+                    if (pendingIntents.size()==0) {
+                        // we've done all the work, free up resources (if not already killed by system)
+                        notificationManager.cancel(9248);
+                        handler.removeCallbacks(this);
+                        waitingThread.quit();
+                        return;
+                    }
                 }
                 handler.postDelayed(this, 5000);
             }
