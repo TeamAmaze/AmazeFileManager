775e8905b6adabf42b22943960ffbde9506b1e1d
Emmanuel <emmanuelbendavid@gmail.com>
2017-04-18 12:47:41 -0300

    Widget works

--- src/main/java/com/amaze/filemanager/ui/views/preference/NamePathSwitchPreference.java
+++ src/main/java/com/amaze/filemanager/ui/views/preference/NamePathSwitchPreference.java
@@ -4,8 +4,7 @@ import android.content.Context;
 import android.preference.Preference;
 import android.util.AttributeSet;
 import android.view.View;
-import android.widget.Checkable;
-import android.widget.Switch;
+import android.view.ViewGroup;
 
 import com.amaze.filemanager.R;
 
@@ -28,25 +27,9 @@ public class NamePathSwitchPreference extends Preference {
         super(context, attrs, defStyle);
     }
 
-    @Override
-    protected void onBindView(View view) {
-        super.onBindView(view);
-
-        View checkableView = view.findViewById(R.id.);
-        if (checkableView != null && checkableView instanceof Checkable) {
-            if (checkableView instanceof Switch) {
-                final Switch switchView = (Switch) checkableView;
-                switchView.setOnCheckedChangeListener(null);
-            }
-
-            ((Checkable) checkableView).setChecked(true);
-
-            if (checkableView instanceof Switch) {
-                final Switch switchView = (Switch) checkableView;
-                switchView.setTextOn("");
-                switchView.setTextOff("");
-                //switchView.setOnCheckedChangeListener(mListener);
-            }
-        }
+    protected View onCreateView(ViewGroup parent) {
+        setWidgetLayoutResource(R.layout.namepathswich_preference);
+        return super.onCreateView(parent);
     }
+
 }
