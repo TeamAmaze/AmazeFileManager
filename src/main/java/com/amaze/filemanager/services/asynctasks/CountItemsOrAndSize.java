399f99617c000350c51a97c1a9691628f59025a9
Emmanuel <emmanuelbendavid@gmail.com>
2017-05-21 14:46:21 -0300

    Added dynamically loading text for size

--- src/main/java/com/amaze/filemanager/services/asynctasks/CountItemsOrAndSize.java
+++ src/main/java/com/amaze/filemanager/services/asynctasks/CountItemsOrAndSize.java
@@ -2,18 +2,21 @@ package com.amaze.filemanager.services.asynctasks;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.support.v4.util.Pair;
 import android.text.format.Formatter;
 import android.widget.TextView;
 
 import com.amaze.filemanager.R;
 import com.amaze.filemanager.filesystem.BaseFile;
+import com.amaze.filemanager.utils.Futils;
+import com.amaze.filemanager.utils.OnProgressUpdate;
 
 /**
  * @author Emmanuel
  *         on 12/5/2017, at 19:40.
  */
 
-public class CountItemsOrAndSize extends AsyncTask<Void, Void, String> {
+public class CountItemsOrAndSize extends AsyncTask<Void, Pair<Integer, Long>, String> {
 
     private Context context;
     private TextView itemsText;
@@ -31,9 +34,15 @@ public class CountItemsOrAndSize extends AsyncTask<Void, Void, String> {
         long fileLength = file.length(context);
 
         if (file.isDirectory(context)) {
-            int x = file.listFiles(context, false).size();
-            items = x + " " + context.getResources().getQuantityString(R.plurals.items, x) + "; "
-                    + Formatter.formatFileSize(context, fileLength);
+            final int x = file.listFiles(context, false).size();
+            long folderSize = Futils.folderSize(file, new OnProgressUpdate<Long>() {
+                @Override
+                public void onUpdate(Long data) {
+                    publishProgress(new Pair<>(x, data));
+                }
+            });
+
+            items = getText(x, folderSize, false);
         } else {
             items = Formatter.formatFileSize(context, fileLength) + (" (" + fileLength + " "
                     + context.getResources().getQuantityString(R.plurals.bytes, (int) fileLength) //truncation is insignificant
@@ -41,6 +50,18 @@ public class CountItemsOrAndSize extends AsyncTask<Void, Void, String> {
         }
 
         return items;
+    }
+
+    @Override
+    protected void onProgressUpdate(Pair<Integer, Long>[] dataArr) {
+        Pair<Integer, Long> data = dataArr[0];
+
+        itemsText.setText(getText(data.first, data.second, true));
+    }
+
+    private String getText(int filesInFolder, long length, boolean loading) {
+        return filesInFolder + " " + context.getResources().getQuantityString(R.plurals.items, filesInFolder) + "; "
+                + (loading? ">":"") + Formatter.formatFileSize(context, length);
     }
 
     protected void onPostExecute(String items) {
