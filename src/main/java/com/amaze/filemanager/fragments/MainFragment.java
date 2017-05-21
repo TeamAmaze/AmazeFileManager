be929c9cd47bd0e285c45a9c54d157c64677aa89
Emmanuel <emmanuelbendavid@gmail.com>
2017-05-21 14:13:41 -0300

    Deleted all item/s translations and redundant String

--- src/main/java/com/amaze/filemanager/fragments/MainFragment.java
+++ src/main/java/com/amaze/filemanager/fragments/MainFragment.java
@@ -161,7 +161,6 @@ public class MainFragment extends android.support.v4.app.Fragment {
 
     private IconUtils icons;
     private View footerView;
-    private String itemsstring;
     private TabHandler tabHandler;
     private LinearLayoutManager mLayoutManager;
     private GridLayoutManager mLayoutManagerGrid;
@@ -285,7 +284,6 @@ public class MainFragment extends android.support.v4.app.Fragment {
         res = getResources();
         pathname = (TextView) getActivity().findViewById(R.id.pathname);
         mFullPath = (TextView) getActivity().findViewById(R.id.fullpath);
-        itemsstring = res.getString(R.string.items);
         apk = new BitmapDrawable(res, BitmapFactory.decodeResource(res, R.drawable.ic_doc_apk_grid));
         mToolbarContainer.setBackgroundColor(MainActivity.currentTab==1 ? skinTwoColor : skin_color);
 
