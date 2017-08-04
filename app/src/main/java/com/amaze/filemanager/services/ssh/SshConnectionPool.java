3fe95b56792326da0471c21f968e6c2efa42a87b
TranceLove <airwave209gt@gmail.com>
2017-08-04 17:22:10 +0800

    Bare bone implementation of SSH connection pool
    It doesn't make sense to connect and disconnect every time in order to
    fetch files off a SSH server, so we need a connection pool where
    connections can be reused.

---
+++ app/src/main/java/com/amaze/filemanager/services/ssh/SshConnectionPool.java
@@ -0,0 +1,94 @@
+package com.amaze.filemanager.services.ssh;
+
+import android.net.Uri;
+import android.util.Log;
+
+import net.schmizz.sshj.SSHClient;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SshConnectionPool
+{
+    private static final String TAG = "SshConnectionPool";
+
+    private static final int SSH_DEFAULT_PORT = 22;
+
+    private static SshConnectionPool instance = null;
+
+    private final Map<String, SSHClient> connections;
+
+    private SshConnectionPool()
+    {
+        connections = new ConcurrentHashMap<String, SSHClient>();
+    }
+
+    public static final SshConnectionPool getInstance()
+    {
+        if(instance == null)
+            instance = new SshConnectionPool();
+
+        return instance;
+    }
+
+    public SSHClient getConnection(String url) throws IOException
+    {
+        SSHClient client = connections.get(url);
+        if(client == null)
+        {
+            client = create(url);
+        }
+        else
+        {
+            if(!validate(client))
+            {
+                Log.d(TAG, "Connection no longer usable. Reconnecting...");
+                expire(client);
+                connections.remove(url);
+                client = create(url);
+            }
+        }
+        return connections.put(url, client);
+    }
+
+    private SSHClient create(String url) throws IOException
+    {
+        return create(Uri.parse(url));
+    }
+
+    private SSHClient create(Uri uri) throws IOException
+    {
+        String host = uri.getHost();
+        int port = uri.getPort();
+        //If the uri is fetched from the app's database storage, we assume it will never be empty
+        String[] userInfo = uri.getUserInfo().split(":");
+        String username = userInfo[0];
+        String password = userInfo.length > 1 ? userInfo[1] : "";
+
+        if(port < 0)
+            port = SSH_DEFAULT_PORT;
+
+        SSHClient client = new SSHClient();
+        client.connect(host, port);
+        client.authPassword(username, password);
+        return client;
+    }
+
+    private boolean validate(SSHClient client)
+    {
+        return client.isConnected() && client.isAuthenticated();
+    }
+
+    private void expire(SSHClient client)
+    {
+        try
+        {
+            client.disconnect();
+        }
+        catch(IOException e)
+        {
+            Log.w(TAG, e);
+        }
+    }
+}
