279c195592feda6c63e8aa0119619095155ea9d1
zholdiyarov_31284 <Diar2507>
2017-08-22 09:21:40 +0600

    removed duplication of licence definition

--- app/src/play/java/com/amaze/filemanager/activities/MainActivity.java
+++ app/src/play/java/com/amaze/filemanager/activities/MainActivity.java
@@ -166,25 +166,6 @@ import static android.os.Build.VERSION.SDK_INT;
 import static com.amaze.filemanager.fragments.preference_fragments.Preffrag.PREFERENCE_SHOW_SIDEBAR_FOLDERS;
 import static com.amaze.filemanager.fragments.preference_fragments.Preffrag.PREFERENCE_SHOW_SIDEBAR_QUICKACCESSES;
 
-/*
- * Copyright (C) 2017 Sanzhar Zholdiyarov <zholdiyarov@gmail.com>
- *
- * This file is part of Amaze File Manager.
- *
- * Amaze File Manager is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 public class MainActivity extends ThemedActivity implements
 		GoogleApiClient.ConnectionCallbacks,
 		GoogleApiClient.OnConnectionFailedListener, OnRequestPermissionsResultCallback,
