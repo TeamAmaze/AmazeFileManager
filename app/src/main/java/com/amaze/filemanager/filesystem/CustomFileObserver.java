f712af1bd1df4235bf98ea1ddd4173b67eb89f24
Emmanuel <emmanuelbendavid@gmail.com>
2017-11-09 17:58:43 -0300

    Created FileObserver hack for Marshmallow

--- app/src/main/java/com/amaze/filemanager/filesystem/CustomFileObserver.java
+++ app/src/main/java/com/amaze/filemanager/filesystem/CustomFileObserver.java
@@ -1,13 +1,22 @@
 package com.amaze.filemanager.filesystem;
 
+import android.os.Build;
 import android.os.FileObserver;
 import android.os.Handler;
-import android.os.Message;
+import android.support.annotation.RequiresApi;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Class which monitors any change in local filesystem and updates the adapter
@@ -25,7 +34,8 @@ public class CustomFileObserver extends FileObserver {
      * Check: http://rswiki.csie.org/lxr/http/source/include/linux/inotify.h?a=m68k#L45
      */
     private static final int IN_IGNORED = 0x00008000;
-    private static final int DEFER_CONSTANT = 5000;
+    private static final int DEFER_CONSTANT_SECONDS = 5;
+    private static final int DEFER_CONSTANT = DEFER_CONSTANT_SECONDS*1000;
     private static final int MASK = CREATE | MOVED_TO | DELETE | MOVED_FROM | DELETE_SELF | MOVE_SELF;
 
     private long lastMessagedTime = 0L;
@@ -34,8 +44,8 @@ public class CustomFileObserver extends FileObserver {
 
     private Handler handler;
     private String path;
-    private ArrayList<String> pathsAdded = new ArrayList<>();
-    private ArrayList<String> pathsRemoved = new ArrayList<>();
+    private final List<String> pathsAdded = Collections.synchronizedList(new ArrayList<>());
+    private final List<String> pathsRemoved = Collections.synchronizedList(new ArrayList<>());
 
     public CustomFileObserver(String path, Handler handler) {
         super(path, MASK);
@@ -52,9 +62,23 @@ public class CustomFileObserver extends FileObserver {
     }
 
     @Override
+    public void startWatching() {
+        if(Build.VERSION.SDK_INT == Build.VERSION_CODES.M) {
+            startPollingSystem();
+        } else {
+            super.startWatching();
+        }
+    }
+
+    @Override
     public void stopWatching() {
         wasStopped = true;
-        super.stopWatching();
+
+        if(Build.VERSION.SDK_INT == Build.VERSION_CODES.M) {
+            stopPollingSystem();
+        } else {
+            super.startWatching();
+        }
     }
 
     @Override
@@ -104,15 +128,86 @@ public class CustomFileObserver extends FileObserver {
         lastMessagedTime = Calendar.getInstance().getTimeInMillis();
         messagingScheduled = false;
 
-        for (String pathAdded : pathsAdded) {
-            handler.obtainMessage(NEW_ITEM, pathAdded).sendToTarget();
+        synchronized (pathsAdded) {
+            for (String pathAdded : pathsAdded) {
+                handler.obtainMessage(NEW_ITEM, pathAdded).sendToTarget();
+            }
         }
         pathsAdded.clear();
 
-        for (String pathRemoved : pathsRemoved) {
-            handler.obtainMessage(DELETED_ITEM, pathRemoved).sendToTarget();
+        synchronized (pathsRemoved) {
+            for (String pathRemoved : pathsRemoved) {
+                handler.obtainMessage(DELETED_ITEM, pathRemoved).sendToTarget();
+            }
         }
         pathsRemoved.clear();
+    }
+
+    /**
+     *  No such file or directory
+     *  From: http://www-numi.fnal.gov/offline_software/srt_public_context/WebDocs/Errors/unix_system_errors.html
+     */
+    private static final int ENOENT = 2, ENOTDIR = 20;
+
+    private ScheduledExecutorService executor = null;
+
+    /**
+     * In Marshmallow FileObserver is broken, this hack will let you know of changes to a directory
+     * every DEFER_CONSTANT_SECONDS seconds, calling onEvent as expected EXCEPT when moving, in such
+     * cases the event will be creation (if moved into) or deletion (if moved out of) or
+     * DELETE_SELF instead of MOVE_SELF.
+     */
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void startPollingSystem() {
+        executor = Executors.newScheduledThreadPool(1);
+        executor.scheduleWithFixedDelay(new FileTimerTask(path, this),
+                DEFER_CONSTANT_SECONDS, DEFER_CONSTANT_SECONDS, TimeUnit.SECONDS); //This doesn't work with milliseconds (don't know why)
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void stopPollingSystem() {
+        executor.shutdown();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private static class FileTimerTask implements Runnable {
+        private FileObserver fileObserver;
+        private String[] files = null;
+        private File file;
+
+        private FileTimerTask(String path, FileObserver fileObserver) {
+            file = new File(path);
+            if(!file.isDirectory()) throw new IllegalArgumentException("Illegal path, you can only watch directories!");
+            files = file.list();
+            this.fileObserver = fileObserver;
+        }
+
+        @Override
+        public void run() {
+            if (!file.exists()) {
+                fileObserver.onEvent(DELETE_SELF, null);
+                return;
+            }
+            if (!file.canRead() || !file.isHidden()) {
+                fileObserver.onEvent(IN_IGNORED, null);
+                return;
+            }
+
+            String[] newFiles = file.list();
+            for (String s : compare(newFiles, files)) {
+                fileObserver.onEvent(CREATE, s);
+            }
+            for (String s : compare(files, newFiles)) {
+                fileObserver.onEvent(DELETE, s);
+            }
+        }
+
+        private HashSet<String> compare(String[] s1, String[] s2) {
+            HashSet<String> set1 = new HashSet<>(Arrays.asList(s1));
+            HashSet<String> set2 = new HashSet<>(Arrays.asList(s2));
+            set1.removeAll(set2);
+            return set1;
+        }
     }
 
 }
