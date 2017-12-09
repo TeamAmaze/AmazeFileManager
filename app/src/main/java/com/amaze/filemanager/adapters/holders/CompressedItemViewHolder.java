992771123b4aa6554cd1b56d7cab700276fb6feb
Emmanuel <emmanuelbendavid@gmail.com>
2017-12-09 03:20:56 -0300

    Made CompressedItemViewHolder immutable

--- app/src/main/java/com/amaze/filemanager/adapters/holders/CompressedItemViewHolder.java
+++ app/src/main/java/com/amaze/filemanager/adapters/holders/CompressedItemViewHolder.java
@@ -15,25 +15,25 @@ import com.amaze.filemanager.ui.views.RoundedImageView;
 
 public class CompressedItemViewHolder extends RecyclerView.ViewHolder {
     // each data item is just a string in this case
-    public RoundedImageView pictureIcon;
-    public ImageView genericIcon, apkIcon;
-    public TextView txtTitle;
-    public TextView txtDesc;
-    public TextView date;
-    public TextView perm;
-    public View rl;
-    public ImageView checkImageView;
+    public final RoundedImageView pictureIcon;
+    public final ImageView genericIcon, apkIcon;
+    public final TextView txtTitle;
+    public final TextView txtDesc;
+    public final TextView date;
+    public final TextView perm;
+    public final View rl;
+    public final ImageView checkImageView;
 
     public CompressedItemViewHolder(View view) {
         super(view);
-        txtTitle = (TextView) view.findViewById(R.id.firstline);
-        pictureIcon = (RoundedImageView) view.findViewById(R.id.picture_icon);
-        genericIcon = (ImageView) view.findViewById(R.id.generic_icon);
+        txtTitle = view.findViewById(R.id.firstline);
+        pictureIcon = view.findViewById(R.id.picture_icon);
+        genericIcon = view.findViewById(R.id.generic_icon);
         rl = view.findViewById(R.id.second);
-        perm = (TextView) view.findViewById(R.id.permis);
-        date = (TextView) view.findViewById(R.id.date);
-        txtDesc = (TextView) view.findViewById(R.id.secondLine);
-        apkIcon = (ImageView) view.findViewById(R.id.apk_icon);
-        checkImageView = (ImageView) view.findViewById(R.id.check_icon);
+        perm = view.findViewById(R.id.permis);
+        date = view.findViewById(R.id.date);
+        txtDesc = view.findViewById(R.id.secondLine);
+        apkIcon = view.findViewById(R.id.apk_icon);
+        checkImageView = view.findViewById(R.id.check_icon);
     }
 }
