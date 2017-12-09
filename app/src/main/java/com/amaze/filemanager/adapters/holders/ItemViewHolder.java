9ef630c346783ed4214036daefa74f1f9cf5210c
Emmanuel <emmanuelbendavid@gmail.com>
2017-12-09 03:25:57 -0300

    Made ItemViewHolder immutable

--- app/src/main/java/com/amaze/filemanager/adapters/holders/ItemViewHolder.java
+++ app/src/main/java/com/amaze/filemanager/adapters/holders/ItemViewHolder.java
@@ -19,36 +19,35 @@ import com.amaze.filemanager.ui.views.RoundedImageView;
 
 public class ItemViewHolder extends RecyclerView.ViewHolder {
     // each data item is just a string in this case
-    public RoundedImageView pictureIcon;
-    public ImageView genericIcon, apkIcon;
-    public ImageView imageView1;
-    public TextView txtTitle;
-    public TextView txtDesc;
-    public TextView date;
-    public TextView perm;
-    public View rl;
-    public TextView genericText;
-    public ImageButton about;
-    public ImageView checkImageView;
-    public ImageView checkImageViewGrid;
+    public final RoundedImageView pictureIcon;
+    public final ImageView genericIcon, apkIcon;
+    public final ImageView imageView1;
+    public final TextView txtTitle;
+    public final TextView txtDesc;
+    public final TextView date;
+    public final TextView perm;
+    public final View rl;
+    public final TextView genericText;
+    public final ImageButton about;
+    public final ImageView checkImageView;
+    public final ImageView checkImageViewGrid;
 
     public ItemViewHolder(View view) {
         super(view);
 
-        txtTitle = (TextView) view.findViewById(R.id.firstline);
-        pictureIcon = (RoundedImageView) view.findViewById(R.id.picture_icon);
+        txtTitle = view.findViewById(R.id.firstline);
+        pictureIcon = view.findViewById(R.id.picture_icon);
         rl = view.findViewById(R.id.second);
-        perm = (TextView) view.findViewById(R.id.permis);
-        date = (TextView) view.findViewById(R.id.date);
-        txtDesc = (TextView) view.findViewById(R.id.secondLine);
-        apkIcon = (ImageView) view.findViewById(R.id.apk_icon);
-        genericText = (TextView) view.findViewById(R.id.generictext);
-        imageView1 = (ImageView) view.findViewById(R.id.icon_thumb);
-        about = (ImageButton) view.findViewById(R.id.properties);
-        checkImageView = (ImageView) view.findViewById(R.id.check_icon);
-        genericIcon = (ImageView) view.findViewById(R.id.generic_icon);
-        checkImageViewGrid = (ImageView) view.findViewById(R.id.check_icon_grid);
+        perm = view.findViewById(R.id.permis);
+        date = view.findViewById(R.id.date);
+        txtDesc = view.findViewById(R.id.secondLine);
+        apkIcon = view.findViewById(R.id.apk_icon);
+        genericText = view.findViewById(R.id.generictext);
+        imageView1 = view.findViewById(R.id.icon_thumb);
+        about = view.findViewById(R.id.properties);
+        checkImageView = view.findViewById(R.id.check_icon);
+        genericIcon = view.findViewById(R.id.generic_icon);
+        checkImageViewGrid = view.findViewById(R.id.check_icon_grid);
     }
-
-
+    
 }
