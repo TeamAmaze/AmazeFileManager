29d0a5fac435ceeefc259a70fc515b733c8bef1c
Emmanuel <emmanuelbendavid@gmail.com>
2018-01-01 21:15:45 -0300

    Fixed small preferences bug

--- app/src/main/java/com/amaze/filemanager/filesystem/HybridFile.java
+++ app/src/main/java/com/amaze/filemanager/filesystem/HybridFile.java
@@ -12,6 +12,7 @@ import com.amaze.filemanager.exceptions.CloudPluginException;
 import com.amaze.filemanager.exceptions.ShellNotRunningException;
 import com.amaze.filemanager.fragments.MainFragment;
 import com.amaze.filemanager.adapters.data.LayoutElementParcelable;
+import com.amaze.filemanager.fragments.preference_fragments.PrefFrag;
 import com.amaze.filemanager.utils.DataUtils;
 import com.amaze.filemanager.utils.OTGUtil;
 import com.amaze.filemanager.utils.OnFileFound;
@@ -79,7 +80,7 @@ public class HybridFile {
         } else if(context == null) {
             mode = OpenMode.FILE;
         } else {
-            boolean rootmode = PreferenceManager.getDefaultSharedPreferences(context).getBoolean("rootMode", false);
+            boolean rootmode = PreferenceManager.getDefaultSharedPreferences(context).getBoolean(PrefFrag.PREFERENCE_ROOTMODE, false);
             if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
                 mode = OpenMode.FILE;
                 if (rootmode && !getFile().canRead()) {
