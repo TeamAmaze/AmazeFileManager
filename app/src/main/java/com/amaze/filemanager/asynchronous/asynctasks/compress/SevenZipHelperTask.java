b799361186317aac8b813cbc4243dc0e4bd65884
N00byKing <N00byKing@hotmail.de>
2018-04-16 21:20:47 +0200

    Add Support for viewing Files in 7z Archives

--- app/src/main/java/com/amaze/filemanager/asynchronous/asynctasks/compress/SevenZipHelperTask.java
+++ app/src/main/java/com/amaze/filemanager/asynchronous/asynctasks/compress/SevenZipHelperTask.java
@@ -1,14 +1,12 @@
 package com.amaze.filemanager.asynchronous.asynctasks.compress;
 
 import com.amaze.filemanager.adapters.data.CompressedObjectParcelable;
-import com.amaze.filemanager.filesystem.compressed.CompressedHelper;
 import com.amaze.filemanager.utils.OnAsyncTaskFinished;
 
 import org.apache.commons.compress.archivers.sevenz.SevenZArchiveEntry;
 import org.apache.commons.compress.archivers.sevenz.SevenZFile;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -33,8 +31,6 @@ public class SevenZipHelperTask extends CompressedHelperTask {
 
             for (SevenZArchiveEntry entry : sevenzFile.getEntries()) {
                 String name = entry.getName();
-                if (name.endsWith(SEPARATOR)) name = name.substring(0, name.length() - 1);
-
                 boolean isInBaseDir = relativePath.equals("") && !name.contains(SEPARATOR);
                 boolean isInRelativeDir = name.contains(SEPARATOR)
                         && name.substring(0, name.lastIndexOf(SEPARATOR)).equals(relativePath);
