2e623c8596932a8508a3970fd6382ebdf9871d21
TranceLove <airwave209gt@gmail.com>
2018-04-25 16:43:12 +0800

    Modernise MaterialDialog.Builder calls

--- app/src/main/java/com/amaze/filemanager/utils/MainActivityHelper.java
+++ app/src/main/java/com/amaze/filemanager/utils/MainActivityHelper.java
@@ -230,21 +230,12 @@ public class MainActivityHelper {
         TextView textView = view.findViewById(R.id.description);
         textView.setText(mainActivity.getString(R.string.needsaccesssummary) + path + mainActivity.getString(R.string.needsaccesssummary1));
         ((ImageView) view.findViewById(R.id.icon)).setImageResource(R.drawable.sd_operate_step);
-        x.positiveText(R.string.open);
-        x.negativeText(R.string.cancel);
-        x.positiveColor(accentColor);
-        x.negativeColor(accentColor);
-        x.callback(new MaterialDialog.ButtonCallback() {
-            @Override
-            public void onPositive(MaterialDialog materialDialog) {
-                triggerStorageAccessFramework();
-            }
-
-            @Override
-            public void onNegative(MaterialDialog materialDialog) {
-                Toast.makeText(mainActivity, R.string.error, Toast.LENGTH_SHORT).show();
-            }
-        });
+        x.positiveText(R.string.open)
+            .negativeText(R.string.cancel)
+            .positiveColor(accentColor)
+            .negativeColor(accentColor)
+            .onPositive((dialog, which)->triggerStorageAccessFramework())
+            .onNegative((dialog, which)->Toast.makeText(mainActivity, R.string.error, Toast.LENGTH_SHORT).show());
         final MaterialDialog y = x.build();
         y.show();
     }
