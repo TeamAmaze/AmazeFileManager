a0262ec98c79acf97fc0f00c03b21352bbcbfec4
KimJeongHaeng <junaa114@gmail.com>
2018-05-07 14:52:09 +0900

    1. extract class 2. FileUtils Class - fileToContentUri method extract to IconUtil class 3. The purpose of fileToContentUri method is to look for a uri, not a calculation based on the icon.     Therefore, it was extracted by IconUtils class.

--- app/src/main/java/com/amaze/filemanager/utils/files/FileUtils.java
+++ app/src/main/java/com/amaze/filemanager/utils/files/FileUtils.java
@@ -491,55 +491,14 @@ public class FileUtils {
         return null;
     }
 
+    /* extract class - fileToContentUri method : extract to IconUtil class
+    The purpose of fileToContentUri method is to look for a uri, not a calculation based on the icon.
+    Therefore, it was extracted by IconUtils class.
+     */
     private static Uri fileToContentUri(Context context, String path, boolean isDirectory, String volume) {
-        final String where = MediaStore.MediaColumns.DATA + " = ?";
-        int mimeType = Icons.getTypeOfFile(path, isDirectory);
 
-        Uri baseUri;
-        String[] projection;
-
-        switch (mimeType) {
-            case Icons.IMAGE:
-                baseUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-                projection = new String[]{BaseColumns._ID};
-                break;
-            case Icons.VIDEO:
-                baseUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
-                projection = new String[]{BaseColumns._ID};
-                break;
-            case Icons.AUDIO:
-                baseUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-                projection = new String[]{BaseColumns._ID};
-                break;
-            default:
-                baseUri = MediaStore.Files.getContentUri(volume);
-                projection = new String[]{BaseColumns._ID, MediaStore.Files.FileColumns.MEDIA_TYPE};
-        }
-
-        ContentResolver cr = context.getContentResolver();
-        Cursor c = cr.query(baseUri, projection, where, new String[]{path}, null);
-        try {
-            if (c != null && c.moveToNext()) {
-                boolean isValid = false;
-                if (mimeType == Icons.IMAGE || mimeType == Icons.VIDEO || mimeType == Icons.AUDIO) {
-                    isValid = true;
-                } else {
-                    int type = c.getInt(c.getColumnIndexOrThrow(MediaStore.Files.FileColumns.MEDIA_TYPE));
-                    isValid = type != 0;
-                }
-
-                if (isValid) {
-                    // Do not force to use content uri for no media files
-                    long id = c.getLong(c.getColumnIndexOrThrow(BaseColumns._ID));
-                    return Uri.withAppendedPath(baseUri, String.valueOf(id));
-                }
-            }
-        } finally {
-            if (c != null) {
-                c.close();
-            }
-        }
-        return null;
+        IconUtil iconUtil = new IconUtil(context, path, isDirectory, volume);
+        return iconUtil.findURIByusingIcon();
     }
 
     /**
