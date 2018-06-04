2451f47e1ba5f6e5ff6cf038d1613c59cb49e277
TranceLove <airwave209gt@gmail.com>
2018-06-04 13:35:21 +0800

    Add GPL license header for touched files (#985)

--- app/src/main/java/com/amaze/filemanager/asynchronous/asynctasks/compress/RarHelperTask.java
+++ app/src/main/java/com/amaze/filemanager/asynchronous/asynctasks/compress/RarHelperTask.java
@@ -1,3 +1,25 @@
+/*
+ * RarHelperTask.java
+ *
+ * Copyright (C) 2015-2018 Arpit Khurana <arpitkh96@gmail.com>, Vishal Nehra <vishalmeham2@gmail.com>,
+ * Emmanuel Messulam<emmanuelbendavid@gmail.com>, Raymond Lai <airwave209gt@gmail.com> and Contributors.
+ *
+ * This file is part of Amaze File Manager.
+ *
+ * Amaze File Manager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.amaze.filemanager.asynchronous.asynctasks.compress;
 
 import com.amaze.filemanager.adapters.data.CompressedObjectParcelable;
@@ -12,9 +34,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
-/**
- * Created by Arpit on 25-01-2015 edited by Emmanuel Messulam<emmanuelbendavid@gmail.com>
- */
 public class RarHelperTask extends CompressedHelperTask {
 
     private String fileLocation;
--- app/src/main/java/com/amaze/filemanager/asynchronous/asynctasks/compress/TarHelperTask.java
+++ app/src/main/java/com/amaze/filemanager/asynchronous/asynctasks/compress/TarHelperTask.java
@@ -1,3 +1,25 @@
+/*
+ * TarHelperTask.java
+ *
+ * Copyright (C) 2017-2018 Emmanuel Messulam<emmanuelbendavid@gmail.com>,
+ * Raymond Lai <airwave209gt@gmail.com>.
+ *
+ * This file is part of Amaze File Manager.
+ *
+ * Amaze File Manager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.amaze.filemanager.asynchronous.asynctasks.compress;
 
 import com.amaze.filemanager.adapters.data.CompressedObjectParcelable;
@@ -13,11 +35,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import static com.amaze.filemanager.filesystem.compressed.CompressedHelper.SEPARATOR;
-
-/**
- * @author Emmanuel Messulam <emmanuelbendavid@gmail.com>
- *         on 2/12/2017, at 00:40.
- */
 
 public class TarHelperTask extends CompressedHelperTask {
 
