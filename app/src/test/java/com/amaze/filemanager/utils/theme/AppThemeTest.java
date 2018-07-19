2473a5673aa4fc3c35427f77f70e1a51c69ecae4
Emmanuel M <emmanuelbendavid@gmail.com>
2018-07-19 20:14:09 -0300

    test: Removed needless "throw Exception" from AppThemeTest

--- app/src/test/java/com/amaze/filemanager/utils/theme/AppThemeTest.java
+++ app/src/test/java/com/amaze/filemanager/utils/theme/AppThemeTest.java
@@ -13,43 +13,43 @@ import static org.junit.Assert.*;
 public class AppThemeTest {
 
     @Test
-    public void getThemeLightTest() throws Exception {
+    public void getThemeLightTest() {
         AppTheme apptheme = AppTheme.getTheme(AppTheme.LIGHT_INDEX);
         assertEquals(AppTheme.LIGHT, apptheme);
     }
 
     @Test
-    public void getThemeDARKTest() throws Exception {
+    public void getThemeDARKTest() {
         AppTheme apptheme = AppTheme.getTheme(AppTheme.DARK_INDEX);
         assertEquals(AppTheme.DARK, apptheme);
     }
 
     @Test
-    public void getThemeTIMEDTest() throws Exception {
+    public void getThemeTIMEDTest() {
         AppTheme apptheme = AppTheme.getTheme(AppTheme.TIME_INDEX);
         assertEquals(AppTheme.TIMED, apptheme);
     }
 
     @Test
-    public void getThemeBLACKTest() throws Exception {
+    public void getThemeBLACKTest() {
         AppTheme apptheme = AppTheme.getTheme(AppTheme.BLACK_INDEX);
         assertEquals(AppTheme.BLACK, apptheme);
     }
 
     @Test
-    public void getMaterialDialogThemeLIGHTTest() throws Exception {
+    public void getMaterialDialogThemeLIGHTTest() {
         AppTheme apptheme = AppTheme.getTheme(AppTheme.LIGHT_INDEX);
         assertEquals(Theme.LIGHT, apptheme.getMaterialDialogTheme());
     }
 
     @Test
-    public void getMaterialDialogThemeDARKTest() throws Exception {
+    public void getMaterialDialogThemeDARKTest() {
         AppTheme apptheme = AppTheme.getTheme(AppTheme.DARK_INDEX);
         assertEquals(Theme.DARK, apptheme.getMaterialDialogTheme());
     }
 
     @Test
-    public void getMaterialDialogThemeTIMEDTest() throws Exception {
+    public void getMaterialDialogThemeTIMEDTest() {
         AppTheme apptheme = AppTheme.getTheme(AppTheme.TIME_INDEX);
         int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
         if (hour <= 6 || hour >= 18) {
@@ -60,25 +60,25 @@ public class AppThemeTest {
     }
 
     @Test
-    public void getMaterialDialogThemeBLACKTest() throws Exception {
+    public void getMaterialDialogThemeBLACKTest() {
         AppTheme apptheme = AppTheme.getTheme(AppTheme.BLACK_INDEX);
         assertEquals(Theme.DARK, apptheme.getMaterialDialogTheme());
     }
 
     @Test
-    public void getSimpleThemeLIGHTTest() throws Exception {
+    public void getSimpleThemeLIGHTTest() {
         AppTheme apptheme  = AppTheme.getTheme(AppTheme.LIGHT_INDEX);
         assertEquals(AppTheme.LIGHT, apptheme.getSimpleTheme());
     }
 
     @Test
-    public void getSimpleThemeDARKTest() throws Exception {
+    public void getSimpleThemeDARKTest() {
         AppTheme apptheme  = AppTheme.getTheme(AppTheme.DARK_INDEX);
         assertEquals(AppTheme.DARK, apptheme.getSimpleTheme());
     }
 
     @Test
-    public void getSimpleThemeTIMEDTest() throws Exception {
+    public void getSimpleThemeTIMEDTest() {
         AppTheme apptheme  = AppTheme.getTheme(AppTheme.TIME_INDEX);
         int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
         if (hour <= 6 || hour >= 18) {
@@ -89,34 +89,34 @@ public class AppThemeTest {
     }
 
     @Test
-    public void getSimpleThemeBLACKTest() throws Exception {
+    public void getSimpleThemeBLACKTest() {
         AppTheme apptheme  = AppTheme.getTheme(AppTheme.BLACK_INDEX);
         assertEquals(AppTheme.BLACK, apptheme.getSimpleTheme());
     }
 
     @Test
-    public void getIdLIGHTTest() throws Exception {
+    public void getIdLIGHTTest() {
         int index = 0;
         AppTheme apptheme = AppTheme.getTheme(index);
         assertEquals(index , apptheme.getId()) ;
     }
 
     @Test
-    public void getIdDARKTest() throws Exception {
+    public void getIdDARKTest() {
         int index = 1;
         AppTheme apptheme = AppTheme.getTheme(index);
         assertEquals(index , apptheme.getId()) ;
     }
 
     @Test
-    public void getIdTIMEDTest() throws Exception {
+    public void getIdTIMEDTest() {
         int index = 2;
         AppTheme apptheme = AppTheme.getTheme(index);
         assertEquals(index , apptheme.getId()) ;
     }
 
     @Test
-    public void getIdBLACKTest() throws Exception {
+    public void getIdBLACKTest() {
         int index = 3;
         AppTheme apptheme = AppTheme.getTheme(index);
         assertEquals(index , apptheme.getId()) ;
