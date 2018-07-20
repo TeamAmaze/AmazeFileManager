0d68f7375540d63d775ad635d98a15915a9d4884
Emmanuel M <emmanuelbendavid@gmail.com>
2018-07-19 21:28:47 -0300

    test: Removed unnecessary string in UtilsTest and corrected capitalization of function

--- app/src/test/java/com/amaze/filemanager/utils/UtilsTest.java
+++ app/src/test/java/com/amaze/filemanager/utils/UtilsTest.java
@@ -6,27 +6,15 @@ import static com.amaze.filemanager.utils.Utils.sanitizeInput;
 import static org.junit.Assert.*;
 
 public class UtilsTest {
-
-    String test;
     @Test
-    public void testsanitizeInput() {  //This function is sanitize the string. It removes ";","|","&&","..." from string.
-
-        test="|a|";
-        assertEquals("a",sanitizeInput(test));  //test the removing of pipe sign from string.
-        test="...a...";
-        assertEquals("a",sanitizeInput(test));  //test the removing of dots from string.
-        test=";a;";
-        assertEquals("a",sanitizeInput(test));  //test the removing of semicolon sign from string.
-        test="&&a&&";
-        assertEquals("a",sanitizeInput(test));  //test the removing of AMP sign from string.
-        test="|a...";
-        assertEquals("a",sanitizeInput(test));   //test the removing of pipe sign and semicolon sign from string.
-        test="an &&apple";
-        assertEquals("an apple",sanitizeInput(test));  //test the removing of AMP sign which are between two words.
-        test="an ...apple";
-        assertEquals("an apple",sanitizeInput(test));  //test the removing of dots which are between two words.
-        test=";an |apple....";
-        assertEquals("an apple.",sanitizeInput(test));  //test the removing of pipe sign and dots which are between two words. And test the fourth dot is not removed.
-
+    public void testSanitizeInput() {  //This function is sanitize the string. It removes ";","|","&&","..." from string.
+        assertEquals("a",sanitizeInput("|a|"));  //test the removing of pipe sign from string.
+        assertEquals("a",sanitizeInput("...a..."));  //test the removing of dots from string.
+        assertEquals("a",sanitizeInput(";a;"));  //test the removing of semicolon sign from string.
+        assertEquals("a",sanitizeInput("&&a&&"));  //test the removing of AMP sign from string.
+        assertEquals("a",sanitizeInput("|a..."));   //test the removing of pipe sign and semicolon sign from string.
+        assertEquals("an apple",sanitizeInput("an &&apple"));  //test the removing of AMP sign which are between two words.
+        assertEquals("an apple",sanitizeInput("an ...apple"));  //test the removing of dots which are between two words.
+        assertEquals("an apple.",sanitizeInput(";an |apple...."));  //test the removing of pipe sign and dots which are between two words. And test the fourth dot is not removed.
     }
 }
