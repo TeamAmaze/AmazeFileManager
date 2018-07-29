207e4d188332033752712ef216e2fd28d78fb489
Mitch West <mitch9378@gmail.com>
2018-07-28 18:02:21 -0600

    Change Storage to Internal Storage and updated icon to reflect this change. #1310

--- app/src/main/java/com/amaze/filemanager/ui/views/drawer/Drawer.java
+++ app/src/main/java/com/amaze/filemanager/ui/views/drawer/Drawer.java
@@ -1,3 +1,25 @@
+/*
+ * Drawer.java
+ *
+ * Copyright (C) 2014-2018 Mitch West<mitch9378dev@gmail.com> and Contributors.
+ *
+ * This file is part of Amaze File Manager.
+ *
+ * Amaze File Manager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 package com.amaze.filemanager.ui.views.drawer;
 
 import android.content.Intent;
@@ -235,11 +257,12 @@ public class Drawer implements NavigationView.OnNavigationItemSelectedListener {
 
             File f = new File(file);
             String name;
-            @DrawableRes int icon1 = R.drawable.ic_sd_storage_white_24dp;
+            @DrawableRes int icon1 = R.drawable.ic_storage_white_48dp;
             if ("/storage/emulated/legacy".equals(file) || "/storage/emulated/0".equals(file) || "/mnt/sdcard".equals(file)) {
                 name = resources.getString(R.string.storage);
             } else if ("/storage/sdcard1".equals(file)) {
                 name = resources.getString(R.string.extstorage);
+                icon1 = R.drawable.ic_sd_storage_white_24dp;
             } else if ("/".equals(file)) {
                 name = resources.getString(R.string.rootdirectory);
                 icon1 = R.drawable.ic_drawer_root_white;
