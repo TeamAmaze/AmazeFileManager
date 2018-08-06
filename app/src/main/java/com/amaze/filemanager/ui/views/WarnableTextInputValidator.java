84efaec85bb81f0e9f19bd39b654dcd6eea3f1b2
Emmanuel M <emmanuelbendavid@gmail.com>
2018-08-06 13:54:10 -0300

    Added icon to warning and error state for WarnableTextInputValidator

--- app/src/main/java/com/amaze/filemanager/ui/views/WarnableTextInputValidator.java
+++ app/src/main/java/com/amaze/filemanager/ui/views/WarnableTextInputValidator.java
@@ -1,12 +1,14 @@
 package com.amaze.filemanager.ui.views;
 
 import android.content.Context;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.StringRes;
 import android.text.Editable;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 
+import com.amaze.filemanager.R;
 import com.amaze.filemanager.utils.SimpleTextWatcher;
 
 public final class WarnableTextInputValidator extends SimpleTextWatcher implements View.OnFocusChangeListener, View.OnTouchListener {
@@ -15,6 +17,7 @@ public final class WarnableTextInputValidator extends SimpleTextWatcher implemen
     private final View mButton;
     private final WarnableTextInputLayout mTextInputLayout;
     private final OnTextValidate mValidator;
+    private @DrawableRes int warningDrawable, errorDrawable;
 
     public WarnableTextInputValidator(Context context, EditText editText,
                                       WarnableTextInputLayout textInputLayout,
@@ -28,6 +31,9 @@ public final class WarnableTextInputValidator extends SimpleTextWatcher implemen
         mButton.setOnTouchListener(this);
         mButton.setEnabled(false);
         mValidator = validator;
+
+        warningDrawable = R.drawable.ic_warning_24dp;
+        errorDrawable = R.drawable.ic_error_24dp;
     }
 
     @Override
@@ -61,19 +67,29 @@ public final class WarnableTextInputValidator extends SimpleTextWatcher implemen
         switch (state.state) {
             case ReturnState.STATE_NORMAL:
                 mTextInputLayout.removeError();
+                setEditTextIcon(null);
                 mButton.setEnabled(true);
                 break;
             case ReturnState.STATE_ERROR:
-                if(!onlySetWarning) mTextInputLayout.setError(mContext.getString(state.text));
+                if(!onlySetWarning) {
+                    mTextInputLayout.setError(mContext.getString(state.text));
+                    setEditTextIcon(errorDrawable);
+                }
                 mButton.setEnabled(false);
                 break;
             case ReturnState.STATE_WARNING:
                 mTextInputLayout.setWarning(state.text);
+                setEditTextIcon(warningDrawable);
                 mButton.setEnabled(true);
                 break;
         }
 
         return state.state;
+    }
+
+    private void setEditTextIcon(@DrawableRes Integer drawable) {
+        @DrawableRes int drawableInt = drawable != null? drawable:0;
+        mEditText.setCompoundDrawablesWithIntrinsicBounds(0,0, drawableInt,0);
     }
 
     public interface OnTextValidate {
