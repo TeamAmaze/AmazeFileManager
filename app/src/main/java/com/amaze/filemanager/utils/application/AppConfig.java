d5de65121c180297791cca836dfa04c2c4acde13
Emmanuel M <emmanuelbendavid@gmail.com>
2018-08-19 16:51:42 -0300

    Removed RequestQueue redundant methods from AppConfig

--- app/src/main/java/com/amaze/filemanager/utils/application/AppConfig.java
+++ app/src/main/java/com/amaze/filemanager/utils/application/AppConfig.java
@@ -186,16 +186,11 @@ public class AppConfig extends GlideApplication {
         return mInstance;
     }
 
-    public RequestQueue getRequestQueue() {
+    public ImageLoader getImageLoader() {
         if (mRequestQueue == null) {
             mRequestQueue = Volley.newRequestQueue(getApplicationContext());
         }
 
-        return mRequestQueue;
-    }
-
-    public ImageLoader getImageLoader() {
-        getRequestQueue();
         if (mImageLoader == null) {
             this.mImageLoader = new ImageLoader(mRequestQueue, new LruBitmapCache());
         }
@@ -219,19 +214,4 @@ public class AppConfig extends GlideApplication {
         return sActivityContext;
     }
 
-    public <T> void addToRequestQueue(Request<T> req, String tag) {
-        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
-        getRequestQueue().add(req);
-    }
-
-    public <T> void addToRequestQueue(Request<T> req) {
-        req.setTag(TAG);
-        getRequestQueue().add(req);
-    }
-
-    public void cancelPendingRequests(Object tag) {
-        if (mRequestQueue != null) {
-            mRequestQueue.cancelAll(tag);
-        }
-    }
 }
