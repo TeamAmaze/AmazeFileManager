0f60098b51d34ec17242c9c8e29ea7429a9bcdd7
TranceLove <airwave209gt@gmail.com>
2018-09-16 19:48:20 +0800

    Add FtpService upload/download file tests
    Also added commons-net dependency for Robolectric tests (previously for Espresso tests only)

--- app/src/test/java/com/amaze/filemanager/asynchronous/services/ftp/FtpServiceTest.java
+++ app/src/test/java/com/amaze/filemanager/asynchronous/services/ftp/FtpServiceTest.java
@@ -4,14 +4,19 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.net.wifi.WifiManager;
 import android.os.Environment;
 import android.preference.PreferenceManager;
+import android.util.Base64;
 
 import com.amaze.filemanager.BuildConfig;
 import com.amaze.filemanager.test.ShadowContentResolverWithLocalOutputStreamSupport;
 
+import org.apache.commons.compress.utils.IOUtils;
+import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -21,17 +26,30 @@ import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadow.api.Shadow;
 import org.robolectric.shadows.ShadowConnectivityManager;
 import org.robolectric.shadows.ShadowNetworkInfo;
 import org.robolectric.shadows.ShadowWifiManager;
 import org.robolectric.shadows.multidex.ShadowMultiDex;
 import org.robolectric.util.ReflectionHelpers;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Writer;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketException;
+import java.nio.charset.Charset;
+import java.security.SecureRandom;
+import java.util.Locale;
 
 import static com.amaze.filemanager.asynchronous.services.ftp.FtpService.KEY_PREFERENCE_PATH;
 import static com.amaze.filemanager.asynchronous.services.ftp.FtpService.KEY_PREFERENCE_PORT;
@@ -176,14 +194,109 @@ public class FtpServiceTest {
         assertEquals("/", ftpClient.printWorkingDirectory());
     }
 
-    private void login() throws Exception {
+    @Test
+    public void testUploadFile() throws IOException {
+        byte[] bytes1 = new byte[32], bytes2 = new byte[32];
+        SecureRandom sr = new SecureRandom();
+        sr.setSeed(System.currentTimeMillis());
+        sr.nextBytes(bytes1);
+        sr.nextBytes(bytes2);
+
+        String randomString = Base64.encodeToString(bytes1, Base64.DEFAULT);
+
+        login();
+        assertEquals("/", ftpClient.printWorkingDirectory());
+        ftpClient.changeWorkingDirectory("/");
+        ftpClient.enterLocalPassiveMode();
+        ftpClient.setFileType(FTP.ASCII_FILE_TYPE);
+        InputStream in = new ByteArrayInputStream(randomString.getBytes("utf-8"));
+        ftpClient.storeFile("test.txt", in);
+        in.close();
+        in = new ByteArrayInputStream(bytes2);
+        ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+        ftpClient.storeFile("test.bin", in);
+        in.close();
+        ftpClient.logout();
+        ftpClient.disconnect();
+
+        File verify = new File(Environment.getExternalStorageDirectory(), "test.txt");
+        assertTrue(verify.exists());
+        ByteArrayOutputStream verifyContent = new ByteArrayOutputStream();
+        IOUtils.copy(new FileInputStream(verify), verifyContent);
+        assertEquals(randomString, verifyContent.toString("utf-8"));
+        verify.delete();
+        verify = new File(Environment.getExternalStorageDirectory(), "test.bin");
+        assertTrue(verify.exists());
+        verifyContent = new ByteArrayOutputStream();
+        IOUtils.copy(new FileInputStream(verify), verifyContent);
+        assertArrayEquals(bytes2, verifyContent.toByteArray());
+        verify.delete();
+    }
+
+    @Test
+    public void testDownloadFile() throws IOException {
+        File testFile1 = new File(Environment.getExternalStorageDirectory(), "test.txt");
+        File testFile2 = new File(Environment.getExternalStorageDirectory(), "test.bin");
+
+        byte[] bytes1 = new byte[32], bytes2 = new byte[32];
+        SecureRandom sr = new SecureRandom();
+        sr.setSeed(System.currentTimeMillis());
+        sr.nextBytes(bytes1);
+        sr.nextBytes(bytes2);
+
+        String randomString = Base64.encodeToString(bytes1, Base64.DEFAULT);
+
+        Writer writer = new FileWriter(testFile1);
+        writer.write(randomString);
+        writer.close();
+
+        OutputStream out = new FileOutputStream(testFile2);
+        out.write(bytes2, 0, bytes2.length);
+        out.close();
+
+        ShadowContentResolverWithLocalOutputStreamSupport contentResolver = Shadow.extract(RuntimeEnvironment.application.getContentResolver());
+        contentResolver.registerInputStream(Uri.fromFile(testFile1), new ByteArrayInputStream(randomString.getBytes(Charset.forName("UTF-8"))));
+        contentResolver.registerInputStream(Uri.fromFile(testFile2), new ByteArrayInputStream(bytes2));
+
+        login();
+        assertEquals("/", ftpClient.printWorkingDirectory());
+        ftpClient.changeWorkingDirectory("/");
+
+        FTPFile[] files = ftpClient.listFiles();
+        assertEquals(2, files.length);
+        for(FTPFile file:files){
+            System.err.println(String.format(Locale.US, "%s %d", file.getName(), file.getSize()));
+        }
+
+        ftpClient.enterLocalPassiveMode();
+        ftpClient.setFileType(FTP.ASCII_FILE_TYPE);
+        ByteArrayOutputStream verify = new ByteArrayOutputStream();
+        ftpClient.retrieveFile("test.txt", verify);
+        verify.close();
+        assertTrue(verify.size() > 0);
+        assertEquals(randomString, verify.toString("utf-8"));
+
+        ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+        verify = new ByteArrayOutputStream();
+        ftpClient.retrieveFile("test.bin", verify);
+        verify.close();
+        assertArrayEquals(bytes2, verify.toByteArray());
+
+        ftpClient.logout();
+        ftpClient.disconnect();
+
+        testFile1.delete();
+        testFile2.delete();
+    }
+
+    private void login() throws IOException {
         this.ftpClient = new FTPClient();
         ftpClient.connect("localhost", FTP_PORT);
         ftpClient.login("anonymous", "test@example.com");
     }
 
     //Required since FTPService is running on separate thread...
-    private void waitForServer() throws Exception {
+    private void waitForServer() throws IOException {
         boolean available = false;
         while(!available) {
             Socket socket = new Socket();
