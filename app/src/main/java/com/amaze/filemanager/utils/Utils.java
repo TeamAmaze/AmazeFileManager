f8fc0435f14fc88c9321242f9ac9a5cc0f2dbe88
Raymond Lai <airwave209gt@gmail.com>
2018-12-31 10:55:51 +0800

    Remove forcing Locale.US in String.format()

--- app/src/main/java/com/amaze/filemanager/utils/Utils.java
+++ app/src/main/java/com/amaze/filemanager/utils/Utils.java
@@ -268,6 +268,6 @@ public class Utils {
     public static String formatTimer(long timerInSeconds) {
         final long min = TimeUnit.SECONDS.toMinutes(timerInSeconds);
         final long sec = TimeUnit.SECONDS.toSeconds(timerInSeconds - TimeUnit.MINUTES.toSeconds(min));
-        return String.format(Locale.US, "%02d:%02d", min, sec);
+        return String.format("%02d:%02d", min, sec);
     }
 }
