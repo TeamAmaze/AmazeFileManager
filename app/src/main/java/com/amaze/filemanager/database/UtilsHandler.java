bdd3493d9a21094ca92c3f02a5eac32c4be7240e
TheZxcv <the.uint64.t@gmail.com>
2019-11-05 22:57:17 +0100

    Fix SFTP password and fingerprint update

--- app/src/main/java/com/amaze/filemanager/database/UtilsHandler.java
+++ app/src/main/java/com/amaze/filemanager/database/UtilsHandler.java
@@ -254,13 +254,14 @@ public class UtilsHandler extends SQLiteOpenHelper {
     }
 
     public void updateSsh(String connectionName, String oldConnectionName, String path,
-                          String sshKeyName, String sshKey) {
+                          String hostKey, String sshKeyName, String sshKey) {
 
         SQLiteDatabase database = getWritableDatabase();
 
         ContentValues values = new ContentValues();
         values.put(COLUMN_NAME, connectionName);
         values.put(COLUMN_PATH, path);
+        values.put(COLUMN_HOST_PUBKEY, hostKey);
         if(sshKeyName != null && sshKey != null) {
             values.put(COLUMN_PRIVATE_KEY_NAME, sshKeyName);
             values.put(COLUMN_PRIVATE_KEY, sshKey);
