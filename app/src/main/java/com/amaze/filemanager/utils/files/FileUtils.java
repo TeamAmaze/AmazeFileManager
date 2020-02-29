b3420fef640d14992f51ba14dd97d8e6ede5dfc0
Emmanuel M <emmanuelbendavid@gmail.com>
2020-02-29 18:37:58 -0300

    Fix dissapearing opened documents' activities

--- app/src/main/java/com/amaze/filemanager/utils/files/FileUtils.java
+++ app/src/main/java/com/amaze/filemanager/utils/files/FileUtils.java
@@ -483,11 +483,9 @@ public class FileUtils {
 
     private static void applyNewDocFlag(Intent i) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            i.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT | Intent.FLAG_ACTIVITY_RETAIN_IN_RECENTS);
         } else {
-
-            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_TASK_ON_HOME
-                | Intent.FLAG_ACTIVITY_RETAIN_IN_RECENTS);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_TASK_ON_HOME);
         }
     }
 
