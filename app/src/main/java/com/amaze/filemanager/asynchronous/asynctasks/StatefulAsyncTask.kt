734d8fe60a2bc34c54824017ff9ffc91a8b270be
VishalNehra <vishalmeham2@gmail.com>
2020-10-13 12:07:37 +0530

    1978: Fix codecy issues

--- app/src/main/java/com/amaze/filemanager/asynchronous/asynctasks/StatefulAsyncTask.kt
+++ app/src/main/java/com/amaze/filemanager/asynchronous/asynctasks/StatefulAsyncTask.kt
@@ -20,6 +20,15 @@
 
 package com.amaze.filemanager.asynchronous.asynctasks
 
+/**
+ * Interface to define state to Asynctask
+ */
 interface StatefulAsyncTask<T> {
+
+    /**
+     * Set callback to current async task. To be used to attach the context on
+     * orientation change of fragment / activity
+     * @param t callback
+     */
     fun setCallback(t: T)
 }
