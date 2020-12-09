37316543b169cd4e43c0ce4467daddd81b31f3d4
Vishnu Sanal. T <t.v.s10123@gmail.com>
2020-12-09 19:13:51 +0530

    Fixed Issue #1467 - When searching, hidden files are shown

--- app/src/main/java/com/amaze/filemanager/asynchronous/asynctasks/SearchAsyncTask.java
+++ app/src/main/java/com/amaze/filemanager/asynchronous/asynctasks/SearchAsyncTask.java
@@ -20,188 +20,188 @@
 
 package com.amaze.filemanager.asynchronous.asynctasks;
 
-import android.app.Activity;
-import android.os.AsyncTask;
-import android.preference.PreferenceManager;
-import android.util.Log;
+import static com.amaze.filemanager.ui.fragments.preference_fragments.PreferencesConstants.PREFERENCE_SHOW_HIDDENFILES;
+
+import java.lang.ref.WeakReference;
+import java.util.regex.Pattern;
 
 import com.amaze.filemanager.filesystem.HybridFile;
 import com.amaze.filemanager.filesystem.HybridFileParcelable;
 import com.amaze.filemanager.ui.fragments.SearchWorkerFragment;
 import com.amaze.filemanager.utils.OpenMode;
 
-import java.lang.ref.WeakReference;
-import java.util.regex.Pattern;
-
-import static com.amaze.filemanager.ui.fragments.preference_fragments.PreferencesConstants.PREFERENCE_SHOW_HIDDENFILES;
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
 public class SearchAsyncTask extends AsyncTask<String, HybridFileParcelable, Void>
-        implements StatefulAsyncTask<SearchWorkerFragment.HelperCallbacks> {
+    implements StatefulAsyncTask<SearchWorkerFragment.HelperCallbacks> {
 
-    private static final String TAG = "SearchAsyncTask";
+  private static final String TAG = "SearchAsyncTask";
 
-    private WeakReference<Activity> activity;
-    private SearchWorkerFragment.HelperCallbacks callbacks;
-    private String input;
-    private OpenMode openMode;
-    private boolean rootMode, isRegexEnabled, isMatchesEnabled;
+  private WeakReference<Activity> activity;
+  private SearchWorkerFragment.HelperCallbacks callbacks;
+  private String input;
+  private OpenMode openMode;
+  private boolean rootMode, isRegexEnabled, isMatchesEnabled;
 
-    public SearchAsyncTask(
-            Activity a, String input, OpenMode openMode, boolean root, boolean regex, boolean matches) {
-        activity = new WeakReference<>(a);
-        this.input = input;
-        this.openMode = openMode;
-        rootMode = root;
-        isRegexEnabled = regex;
-        isMatchesEnabled = matches;
-    }
+  public SearchAsyncTask(
+      Activity a, String input, OpenMode openMode, boolean root, boolean regex, boolean matches) {
+    activity = new WeakReference<>(a);
+    this.input = input;
+    this.openMode = openMode;
+    rootMode = root;
+    isRegexEnabled = regex;
+    isMatchesEnabled = matches;
+  }
 
-    @Override
-    protected void onPreExecute() {
-        /*
-         * Note that we need to check if the callbacks are null in each
-         * method in case they are invoked after the Activity's and
-         * Fragment's onDestroy() method have been called.
-         */
-        if (callbacks != null) {
-            callbacks.onPreExecute(input);
-        }
-    }
-
-    // callbacks not checked for null because of possibility of
-    // race conditions b/w worker thread main thread
-    @Override
-    protected Void doInBackground(String... params) {
-        String path = params[0];
-        HybridFile file = new HybridFile(openMode, path);
-        file.generateMode(activity.get());
-        if (file.isSmb()) return null;
-
-        // level 1
-        // if regex or not
-        if (!isRegexEnabled) {
-            search(file, input);
-        } else {
-            // compile the regular expression in the input
-            Pattern pattern = Pattern.compile(bashRegexToJava(input));
-            // level 2
-            if (!isMatchesEnabled) searchRegExFind(file, pattern);
-            else searchRegExMatch(file, pattern);
-        }
-        return null;
-    }
-
-    @Override
-    public void onPostExecute(Void c) {
-        if (callbacks != null) {
-            callbacks.onPostExecute(input);
-        }
-    }
-
-    @Override
-    protected void onCancelled() {
-        if (callbacks != null) callbacks.onCancelled();
-    }
-
-    @Override
-    public void onProgressUpdate(HybridFileParcelable... val) {
-        if (!isCancelled() && callbacks != null) {
-            callbacks.onProgressUpdate(val[0], input);
-        }
-    }
-
-    @Override
-    public void setCallback(SearchWorkerFragment.HelperCallbacks helperCallbacks) {
-        this.callbacks = helperCallbacks;
-    }
-
-    /**
-     * Recursively search for occurrences of a given text in file names and publish the result
-     *
-     * @param directory the current path
+  @Override
+  protected void onPreExecute() {
+    /*
+     * Note that we need to check if the callbacks are null in each
+     * method in case they are invoked after the Activity's and
+     * Fragment's onDestroy() method have been called.
      */
-    private void search(HybridFile directory, final SearchFilter filter) {
-        if (directory.isDirectory(activity.get())) { // do you have permission to read this directory?
-            directory.forEachChildrenFile(
-                    activity.get(),
-                    rootMode,
-                    file -> {
-                        boolean showHiddenFiles =
-                                PreferenceManager.getDefaultSharedPreferences(activity.get())
-                                        .getBoolean(PREFERENCE_SHOW_HIDDENFILES, false);
-
-                        if (!isCancelled() && (showHiddenFiles || !(!showHiddenFiles && file.isHidden()))) {
-                            if (filter.searchFilter(file.getName(activity.get()))) {
-                                publishProgress(file);
-                            }
-                            if (file.isDirectory() && !isCancelled()) {
-                                search(file, filter);
-                            }
-                        }
-                    });
-        } else {
-            Log.d(TAG, "Cannot search " + directory.getPath() + ": Permission Denied");
-        }
+    if (callbacks != null) {
+      callbacks.onPreExecute(input);
     }
+  }
 
-    /**
-     * Recursively search for occurrences of a given text in file names and publish the result
-     *
-     * @param file  the current path
-     * @param query the searched text
-     */
-    private void search(HybridFile file, final String query) {
-        search(file, fileName -> fileName.toLowerCase().contains(query.toLowerCase()));
+  // callbacks not checked for null because of possibility of
+  // race conditions b/w worker thread main thread
+  @Override
+  protected Void doInBackground(String... params) {
+    String path = params[0];
+    HybridFile file = new HybridFile(openMode, path);
+    file.generateMode(activity.get());
+    if (file.isSmb()) return null;
+
+    // level 1
+    // if regex or not
+    if (!isRegexEnabled) {
+      search(file, input);
+    } else {
+      // compile the regular expression in the input
+      Pattern pattern = Pattern.compile(bashRegexToJava(input));
+      // level 2
+      if (!isMatchesEnabled) searchRegExFind(file, pattern);
+      else searchRegExMatch(file, pattern);
     }
+    return null;
+  }
 
-    /**
-     * Recursively find a java regex pattern {@link Pattern} in the file names and publish the result
-     *
-     * @param file    the current file
-     * @param pattern the compiled java regex
-     */
-    private void searchRegExFind(HybridFile file, final Pattern pattern) {
-        search(file, fileName -> pattern.matcher(fileName).find());
+  @Override
+  public void onPostExecute(Void c) {
+    if (callbacks != null) {
+      callbacks.onPostExecute(input);
     }
+  }
 
-    /**
-     * Recursively match a java regex pattern {@link Pattern} with the file names and publish the
-     * result
-     *
-     * @param file    the current file
-     * @param pattern the compiled java regex
-     */
-    private void searchRegExMatch(HybridFile file, final Pattern pattern) {
-        search(file, fileName -> pattern.matcher(fileName).matches());
+  @Override
+  protected void onCancelled() {
+    if (callbacks != null) callbacks.onCancelled();
+  }
+
+  @Override
+  public void onProgressUpdate(HybridFileParcelable... val) {
+    if (!isCancelled() && callbacks != null) {
+      callbacks.onProgressUpdate(val[0], input);
     }
+  }
 
-    /**
-     * method converts bash style regular expression to java. See {@link Pattern}
-     *
-     * @return converted string
-     */
-    private String bashRegexToJava(String originalString) {
-        StringBuilder stringBuilder = new StringBuilder();
+  @Override
+  public void setCallback(SearchWorkerFragment.HelperCallbacks helperCallbacks) {
+    this.callbacks = helperCallbacks;
+  }
 
-        for (int i = 0; i < originalString.length(); i++) {
-            switch (originalString.charAt(i) + "") {
-                case "*":
-                    stringBuilder.append("\\w*");
-                    break;
-                case "?":
-                    stringBuilder.append("\\w");
-                    break;
-                default:
-                    stringBuilder.append(originalString.charAt(i));
-                    break;
+  /**
+   * Recursively search for occurrences of a given text in file names and publish the result
+   *
+   * @param directory the current path
+   */
+  private void search(HybridFile directory, final SearchFilter filter) {
+    if (directory.isDirectory(activity.get())) { // do you have permission to read this directory?
+      directory.forEachChildrenFile(
+          activity.get(),
+          rootMode,
+          file -> {
+            boolean showHiddenFiles =
+                PreferenceManager.getDefaultSharedPreferences(activity.get())
+                    .getBoolean(PREFERENCE_SHOW_HIDDENFILES, false);
+
+            if (!isCancelled() && (showHiddenFiles || !(!showHiddenFiles && file.isHidden()))) {
+              if (filter.searchFilter(file.getName(activity.get()))) {
+                publishProgress(file);
+              }
+              if (file.isDirectory() && !isCancelled()) {
+                search(file, filter);
+              }
             }
-        }
+          });
+    } else {
+      Log.d(TAG, "Cannot search " + directory.getPath() + ": Permission Denied");
+    }
+  }
 
-        Log.d(getClass().getSimpleName(), stringBuilder.toString());
-        return stringBuilder.toString();
+  /**
+   * Recursively search for occurrences of a given text in file names and publish the result
+   *
+   * @param file the current path
+   * @param query the searched text
+   */
+  private void search(HybridFile file, final String query) {
+    search(file, fileName -> fileName.toLowerCase().contains(query.toLowerCase()));
+  }
+
+  /**
+   * Recursively find a java regex pattern {@link Pattern} in the file names and publish the result
+   *
+   * @param file the current file
+   * @param pattern the compiled java regex
+   */
+  private void searchRegExFind(HybridFile file, final Pattern pattern) {
+    search(file, fileName -> pattern.matcher(fileName).find());
+  }
+
+  /**
+   * Recursively match a java regex pattern {@link Pattern} with the file names and publish the
+   * result
+   *
+   * @param file the current file
+   * @param pattern the compiled java regex
+   */
+  private void searchRegExMatch(HybridFile file, final Pattern pattern) {
+    search(file, fileName -> pattern.matcher(fileName).matches());
+  }
+
+  /**
+   * method converts bash style regular expression to java. See {@link Pattern}
+   *
+   * @return converted string
+   */
+  private String bashRegexToJava(String originalString) {
+    StringBuilder stringBuilder = new StringBuilder();
+
+    for (int i = 0; i < originalString.length(); i++) {
+      switch (originalString.charAt(i) + "") {
+        case "*":
+          stringBuilder.append("\\w*");
+          break;
+        case "?":
+          stringBuilder.append("\\w");
+          break;
+        default:
+          stringBuilder.append(originalString.charAt(i));
+          break;
+      }
     }
 
-    public interface SearchFilter {
-        boolean searchFilter(String fileName);
-    }
+    Log.d(getClass().getSimpleName(), stringBuilder.toString());
+    return stringBuilder.toString();
+  }
+
+  public interface SearchFilter {
+    boolean searchFilter(String fileName);
+  }
 }
