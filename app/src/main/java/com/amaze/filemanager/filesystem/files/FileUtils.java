e999813b8ab009a3f7243b5cc9b613c62afb8686
Raymond Lai <airwave209gt@gmail.com>
2021-01-04 18:09:12 +0800

    Changes per PR feedback
    - Simplified condition check for audio+easter egg
    - Removed unnecessary Intent creation

--- app/src/main/java/com/amaze/filemanager/filesystem/files/FileUtils.java
+++ app/src/main/java/com/amaze/filemanager/filesystem/files/FileUtils.java
@@ -600,8 +600,8 @@ public class FileUtils {
     boolean useNewStack =
         sharedPrefs.getBoolean(PreferencesConstants.PREFERENCE_TEXTEDITOR_NEWSTACK, false);
     boolean defaultHandler = isSelfDefault(f, m);
-    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(m);
     final Toast[] studioCount = {null};
+    final int studio_count = PreferenceManager.getDefaultSharedPreferences(m).getInt("studio", 0);
 
     if (f.getName().toLowerCase().endsWith(".apk")) {
       GeneralDialogCreation.showPackageDialog(f, m);
@@ -611,35 +611,26 @@ public class FileUtils {
       Intent intent = new Intent(m, DatabaseViewerActivity.class);
       intent.putExtra("path", f.getPath());
       m.startActivity(intent);
-    } else if (Icons.getTypeOfFile(f.getPath(), f.isDirectory()) == Icons.AUDIO) {
-      final int studio_count = sharedPreferences.getInt("studio", 0);
-      Uri uri = Uri.fromFile(f);
-      final Intent intent = new Intent();
-      intent.setAction(Intent.ACTION_VIEW);
-      intent.setDataAndType(uri, "audio/*");
-
+    } else if (Icons.getTypeOfFile(f.getPath(), f.isDirectory()) == Icons.AUDIO
+        && studio_count != 0) {
       // Behold! It's the  legendary easter egg!
-      if (studio_count != 0) {
-        new CountDownTimer(studio_count, 1000) {
-          @Override
-          public void onTick(long millisUntilFinished) {
-            int sec = (int) millisUntilFinished / 1000;
-            if (studioCount[0] != null) studioCount[0].cancel();
-            studioCount[0] = Toast.makeText(m, sec + "", Toast.LENGTH_LONG);
-            studioCount[0].show();
-          }
+      new CountDownTimer(studio_count, 1000) {
+        @Override
+        public void onTick(long millisUntilFinished) {
+          int sec = (int) millisUntilFinished / 1000;
+          if (studioCount[0] != null) studioCount[0].cancel();
+          studioCount[0] = Toast.makeText(m, sec + "", Toast.LENGTH_LONG);
+          studioCount[0].show();
+        }
 
-          @Override
-          public void onFinish() {
-            if (studioCount[0] != null) studioCount[0].cancel();
-            studioCount[0] = Toast.makeText(m, m.getString(R.string.opening), Toast.LENGTH_LONG);
-            studioCount[0].show();
-            openFileDialogFragmentFor(f, m, "audio/*");
-          }
-        }.start();
-      } else {
-        openFileDialogFragmentFor(f, m);
-      }
+        @Override
+        public void onFinish() {
+          if (studioCount[0] != null) studioCount[0].cancel();
+          studioCount[0] = Toast.makeText(m, m.getString(R.string.opening), Toast.LENGTH_LONG);
+          studioCount[0].show();
+          openFileDialogFragmentFor(f, m, "audio/*");
+        }
+      }.start();
     } else {
       try {
         openFileDialogFragmentFor(f, m);
