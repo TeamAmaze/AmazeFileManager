24e0a944f2e1ea3793a0f8391d0962d5b6a5eb65
Raymond Lai <airwave209gt@gmail.com>
2021-04-25 22:56:38 +0800

    Rebase with upstream and changes per feedback
    - Removed redundant methods in UserColorPreferences
    - Made ScreenUtils.activity getter nullable

--- app/src/main/java/com/amaze/filemanager/utils/ScreenUtils.kt
+++ app/src/main/java/com/amaze/filemanager/utils/ScreenUtils.kt
@@ -28,34 +28,38 @@ import kotlin.math.roundToInt
 class ScreenUtils(act: Activity) {
 
     private val _activity: WeakReference<Activity> = WeakReference(act)
-    private val activity: Activity
-        get() = _activity.get()!!
+    private val activity: Activity?
+        get() = _activity.get()
 
     /**
      * Converts Density Pixels to real Pixels in screen
      * It uses context to retrieve the density.
      */
     fun convertDbToPx(dp: Float): Int =
-        (activity.resources.displayMetrics.density * dp).roundToInt()
+        activity?.let {
+            (it.resources.displayMetrics.density * dp).roundToInt()
+        } ?: 0
 
     /**
      * Converts real Pixels in screen to Density Pixels
      * It uses context to retrieve the density.
      */
     fun convertPxToDb(px: Float): Int =
-        (px / activity.resources.displayMetrics.density).roundToInt()
+        activity?.let {
+            (px / it.resources.displayMetrics.density).roundToInt()
+        } ?: 0
 
     private val screenWidthInPx: Int
         get() {
             val displayMetrics = DisplayMetrics()
-            activity.windowManager.defaultDisplay.getMetrics(displayMetrics)
+            activity?.windowManager?.defaultDisplay?.getMetrics(displayMetrics)
             return displayMetrics.widthPixels
         }
 
     private val screenHeightInPx: Int
         get() {
             val displayMetrics = DisplayMetrics()
-            activity.windowManager.defaultDisplay.getMetrics(displayMetrics)
+            activity?.windowManager?.defaultDisplay?.getMetrics(displayMetrics)
             return displayMetrics.heightPixels
         }
 
