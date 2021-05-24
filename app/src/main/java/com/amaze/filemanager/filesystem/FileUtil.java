cd078d733e0f0fda460173e978f62f9ad25a2972
EmmanuelMess <emmanuelbendavid@gmail.com>
2021-05-24 01:58:47 -0300

    Delete unused FileUtil.copyDummyFile function

--- app/src/main/java/com/amaze/filemanager/filesystem/FileUtil.java
+++ app/src/main/java/com/amaze/filemanager/filesystem/FileUtil.java
@@ -30,10 +30,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.List;
@@ -58,11 +55,8 @@ import android.annotation.TargetApi;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
-import android.os.ParcelFileDescriptor;
-import android.provider.BaseColumns;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.Toast;
@@ -814,55 +808,6 @@ public abstract class FileUtil {
   // Utility methods for Kitkat
 
   /**
-   * Copy a resource file into a private target directory, if the target does not yet exist.
-   * Required for the Kitkat workaround.
-   *
-   * @param resource The resource file.
-   * @param folderName The folder below app folder where the file is copied to.
-   * @param targetName The name of the target file.
-   * @return the dummy file.
-   */
-  private static File copyDummyFile(
-      final int resource, final String folderName, final String targetName, Context context)
-      throws IOException {
-    File externalFilesDir = context.getExternalFilesDir(folderName);
-    if (externalFilesDir == null) {
-      return null;
-    }
-    File targetFile = new File(externalFilesDir, targetName);
-
-    if (!targetFile.exists()) {
-      InputStream in = null;
-      OutputStream out = null;
-      try {
-        in = context.getResources().openRawResource(resource);
-        out = new FileOutputStream(targetFile);
-        byte[] buffer = new byte[4096]; // MAGIC_NUMBER
-        int bytesRead;
-        while ((bytesRead = in.read(buffer)) != -1) {
-          out.write(buffer, 0, bytesRead);
-        }
-      } finally {
-        if (in != null) {
-          try {
-            in.close();
-          } catch (IOException ex) {
-            // do nothing
-          }
-        }
-        if (out != null) {
-          try {
-            out.close();
-          } catch (IOException ex) {
-            // do nothing
-          }
-        }
-      }
-    }
-    return targetFile;
-  }
-
-  /**
    * Checks whether the target path exists or is writable
    *
    * @param f the target path
@@ -898,23 +843,6 @@ public abstract class FileUtil {
       return 0;
     }
     return 0;
-  }
-
-  /**
-   * Copy the dummy image and dummy mp3 into the private folder, if not yet there. Required for the
-   * Kitkat workaround.
-   *
-   * @return the dummy mp3.
-   */
-  private static File copyDummyFiles(Context c) {
-    try {
-      copyDummyFile(R.mipmap.ic_launcher, "mkdirFiles", "albumart.jpg", c);
-      return copyDummyFile(R.raw.temptrack, "mkdirFiles", "temptrack.mp3", c);
-
-    } catch (IOException e) {
-      Log.e(LOG, "Could not copy dummy files.", e);
-      return null;
-    }
   }
 
   /**
