191c5172948c6863e31222e5e52b0b0225b809b5
Houssem Nasri <housi.housi2015@gmail.com>
2021-07-03 09:56:53 +0100

    Fix split apks broken icons

--- app/src/main/java/com/amaze/filemanager/adapters/glide/apkimage/ApkImageModelLoaderFactory.java
+++ app/src/main/java/com/amaze/filemanager/adapters/glide/apkimage/ApkImageModelLoaderFactory.java
@@ -24,21 +24,21 @@ import com.bumptech.glide.load.model.ModelLoader;
 import com.bumptech.glide.load.model.ModelLoaderFactory;
 import com.bumptech.glide.load.model.MultiModelLoaderFactory;
 
-import android.content.pm.PackageManager;
+import android.content.Context;
 import android.graphics.drawable.Drawable;
 
 /** @author Emmanuel Messulam <emmanuelbendavid@gmail.com> on 10/12/2017, at 16:21. */
 public class ApkImageModelLoaderFactory implements ModelLoaderFactory<String, Drawable> {
 
-  private PackageManager packageManager;
+  private Context context;
 
-  public ApkImageModelLoaderFactory(PackageManager packageManager) {
-    this.packageManager = packageManager;
+  public ApkImageModelLoaderFactory(Context context) {
+    this.context = context;
   }
 
   @Override
   public ModelLoader<String, Drawable> build(MultiModelLoaderFactory multiFactory) {
-    return new ApkImageModelLoader(packageManager);
+    return new ApkImageModelLoader(context);
   }
 
   @Override
