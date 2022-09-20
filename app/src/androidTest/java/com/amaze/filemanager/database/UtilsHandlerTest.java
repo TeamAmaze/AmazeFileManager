92fee71f5c8c92507033745d4972e56bfc7597b7
TranceLove <airwave209gt@gmail.com>
2022-09-20 14:33:16 +0800

    Fix failing Espresso tests

--- app/src/androidTest/java/com/amaze/filemanager/database/UtilsHandlerTest.java
+++ app/src/androidTest/java/com/amaze/filemanager/database/UtilsHandlerTest.java
@@ -137,6 +137,6 @@ public class UtilsHandlerTest {
     assertEquals(encryptedPath, result.get(0)[1]);
     assertEquals(
         "00:11:22:33:44:55:66:77:88:99:aa:bb:cc:dd:ee:ff",
-        utilsHandler.getSshHostKey(encryptedPath));
+        utilsHandler.getRemoteHostKey(encryptedPath));
   }
 }
