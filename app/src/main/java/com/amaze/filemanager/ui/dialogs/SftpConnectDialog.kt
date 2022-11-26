ff511045986670af9a5226fdb6238c30138ba361
VishnuSanal <t.v.s10123@gmail.com>
2022-11-26 12:56:45 +0530

    fixes #3085

--- app/src/main/java/com/amaze/filemanager/ui/dialogs/SftpConnectDialog.kt
+++ app/src/main/java/com/amaze/filemanager/ui/dialogs/SftpConnectDialog.kt
@@ -692,7 +692,12 @@ class SftpConnectDialog : DialogFragment() {
         hostKeyFingerprint: String?,
         encryptedPath: String
     ): Boolean {
-        DataUtils.getInstance().removeServer(DataUtils.getInstance().containsServer(oldPath))
+        val i = DataUtils.getInstance().containsServer(oldPath)
+
+        if (i != -1) {
+            DataUtils.getInstance().removeServer(i)
+        }
+
         DataUtils.getInstance().addServer(arrayOf(connectionName, encryptedPath))
         Collections.sort(DataUtils.getInstance().servers, BookSorter())
         (activity as MainActivity).drawer.refreshDrawer()
