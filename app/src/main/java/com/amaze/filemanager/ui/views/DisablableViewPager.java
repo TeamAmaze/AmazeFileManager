53074841d6635475203cd31f731378c382a63816
VishnuSanal <t.v.s10123@gmail.com>
2023-01-02 20:13:02 +0530

    chore: run spotless with the updated spotless license

--- app/src/main/java/com/amaze/filemanager/ui/views/DisablableViewPager.java
+++ app/src/main/java/com/amaze/filemanager/ui/views/DisablableViewPager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014-2020 Arpit Khurana <arpitkh96@gmail.com>, Vishal Nehra <vishalmeham2@gmail.com>,
+ * Copyright (C) 2014-2023 Arpit Khurana <arpitkh96@gmail.com>, Vishal Nehra <vishalmeham2@gmail.com>,
  * Emmanuel Messulam<emmanuelbendavid@gmail.com>, Raymond Lai <airwave209gt at gmail.com> and Contributors.
  *
  * This file is part of Amaze File Manager.
