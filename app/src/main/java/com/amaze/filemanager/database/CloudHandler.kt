9d6a9f4ae366adda56e54aa8a67da2e0a927f4f6
Oleksandr Narvatov <hipi96222@gmail.com>
2023-05-28 17:17:40 +0300

    Add contributor to license declaration

--- app/src/main/java/com/amaze/filemanager/database/CloudHandler.kt
+++ app/src/main/java/com/amaze/filemanager/database/CloudHandler.kt
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2014-2023 Arpit Khurana <arpitkh96@gmail.com>, Vishal Nehra <vishalmeham2@gmail.com>,
- * Emmanuel Messulam<emmanuelbendavid@gmail.com>, Raymond Lai <airwave209gt at gmail.com> and Contributors.
+ * Emmanuel Messulam<emmanuelbendavid@gmail.com>, Raymond Lai <airwave209gt at gmail.com>,
+ * Oleksandr Narvatov <hipi96222@gmail.com> and Contributors.
  *
  * This file is part of Amaze File Manager.
  *
