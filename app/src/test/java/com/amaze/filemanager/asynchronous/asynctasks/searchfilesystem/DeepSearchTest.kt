56e0bf919a37475b83b8f0e0003bbae1be1993ee
Selina Lin <dev@lin.tf>
2024-01-19 22:41:36 +0100

    add test for all search modes

---
+++ app/src/test/java/com/amaze/filemanager/asynchronous/asynctasks/searchfilesystem/DeepSearchTest.kt
@@ -0,0 +1,174 @@
+/*
+ * Copyright (C) 2014-2024 Arpit Khurana <arpitkh96@gmail.com>, Vishal Nehra <vishalmeham2@gmail.com>,
+ * Emmanuel Messulam<emmanuelbendavid@gmail.com>, Raymond Lai <airwave209gt at gmail.com> and Contributors.
+ *
+ * This file is part of Amaze File Manager.
+ *
+ * Amaze File Manager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.amaze.filemanager.asynchronous.asynctasks.searchfilesystem
+
+import android.content.Context
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.amaze.filemanager.fileoperations.filesystem.OpenMode
+import com.amaze.filemanager.filesystem.HybridFile
+import com.amaze.filemanager.filesystem.HybridFileParcelable
+import com.amaze.filemanager.filesystem.root.ListFilesCommand
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockkConstructor
+import io.mockk.mockkObject
+import io.mockk.unmockkAll
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import java.util.EnumSet
+
+class DeepSearchTest {
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @MockK(relaxed = true, relaxUnitFun = true)
+    lateinit var context: Context
+
+    @MockK(relaxed = true, relaxUnitFun = true)
+    lateinit var foundFileMock: HybridFileParcelable
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this, relaxUnitFun = true)
+        every { context.applicationContext } returns context
+
+        mockkConstructor(HybridFile::class)
+        every { anyConstructed<HybridFile>().isDirectory(any()) } returns true
+
+        mockkObject(ListFilesCommand)
+        every { ListFilesCommand.listFiles(any(), any(), any(), any(), any()) } answers {
+            val onFileFoundCallback = it.invocation.args.last() as (HybridFileParcelable) -> Unit
+            onFileFoundCallback(foundFileMock)
+        }
+
+        every { foundFileMock.isDirectory(any()) } returns false
+        every { foundFileMock.isHidden } returns false
+    }
+
+    @After
+    fun cleanup() {
+        unmockkAll()
+    }
+
+    /**
+     * If the file name matches the query, the file should be added to
+     * [FileSearch.foundFilesLiveData]
+     */
+    @Test
+    fun testSimpleSearchMatch() {
+        val filePath = "/test/abc.txt"
+        val fileName = "abc.txt"
+
+        every { foundFileMock.path } returns filePath
+        every { foundFileMock.getName(any()) } returns fileName
+
+        val deepSearch = DeepSearch(
+            "ab",
+            filePath,
+            EnumSet.noneOf(SearchParameter::class.java),
+            context,
+            OpenMode.FILE
+        )
+
+        val expectedMatchRanges = listOf(0..1)
+
+        deepSearch.foundFilesLiveData.observeForever { actualResults ->
+            Assert.assertNotNull(actualResults)
+            Assert.assertEquals(listOf(foundFileMock), actualResults.map { it.file })
+            Assert.assertEquals(expectedMatchRanges, actualResults.map { it.matchRange })
+        }
+
+        runTest {
+            deepSearch.search()
+        }
+    }
+
+    /**
+     * If the file name does not match the query, the file should not be added to
+     * [FileSearch.foundFilesLiveData]
+     */
+    @Test
+    fun testSimpleSearchNotMatch() {
+        val filePath = "/test/abc.txt"
+        val fileName = "abc.txt"
+
+        every { foundFileMock.path } returns filePath
+        every { foundFileMock.getName(any()) } returns fileName
+
+        val deepSearch = DeepSearch(
+            "ba",
+            filePath,
+            EnumSet.noneOf(SearchParameter::class.java),
+            context,
+            OpenMode.FILE
+        )
+
+        deepSearch.foundFilesLiveData.observeForever { actualResults ->
+            Assert.assertNotNull(actualResults)
+            Assert.assertTrue(
+                "List was not empty as expected but had ${actualResults.size} elements",
+                actualResults.isEmpty()
+            )
+        }
+
+        runTest {
+            deepSearch.search()
+        }
+    }
+
+    /**
+     * If the match is in the path but not in the name, it should not be added to
+     * [FileSearch.foundFilesLiveData]
+     */
+    @Test
+    fun testSearchWithPathMatchButNameNotMatch() {
+        val filePath = "/test/abc.txt"
+        val fileName = "abc.txt"
+
+        every { foundFileMock.path } returns filePath
+        every { foundFileMock.getName(any()) } returns fileName
+
+        val deepSearch = DeepSearch(
+            "test",
+            filePath,
+            EnumSet.noneOf(SearchParameter::class.java),
+            context,
+            OpenMode.FILE
+        )
+
+        deepSearch.foundFilesLiveData.observeForever { actualResults ->
+            Assert.assertNotNull(actualResults)
+            Assert.assertTrue(
+                "List was not empty as expected but had ${actualResults.size} elements",
+                actualResults.isEmpty()
+            )
+        }
+
+        runTest {
+            deepSearch.search()
+        }
+    }
+}
