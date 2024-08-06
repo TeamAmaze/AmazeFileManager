6dcedd2666959f6f4876a5ea9c0898efedd96f3d
VishnuSanal <t.v.s10123@gmail.com>
2024-08-06 23:48:36 +0530

    remove migrator & use auth client directly

--- app/src/main/java/com/amaze/filemanager/utils/omh/OMHClientHelper.kt
+++ app/src/main/java/com/amaze/filemanager/utils/omh/OMHClientHelper.kt
@@ -3,13 +3,13 @@ package com.amaze.filemanager.utils.omh
 import android.content.Context
 import android.content.Intent
 import com.amaze.filemanager.BuildConfig
-import com.omh.android.auth.api.async.OmhCancellable
-import com.omh.android.auth.api.async.OmhTask
-import com.omh.android.auth.api.models.OmhUserProfile
-import com.omh.android.storage.api.OmhStorageClient
-import com.omh.android.storage.api.OmhStorageProvider
 import com.openmobilehub.android.auth.core.OmhAuthClient
 import com.openmobilehub.android.auth.core.OmhAuthProvider
+import com.openmobilehub.android.auth.core.async.OmhCancellable
+import com.openmobilehub.android.auth.core.async.OmhTask
+import com.openmobilehub.android.auth.core.models.OmhUserProfile
+import com.openmobilehub.android.storage.core.OmhStorageClient
+import com.openmobilehub.android.storage.core.OmhStorageProvider
 
 class OMHClientHelper {
     companion object {
@@ -61,66 +61,16 @@ class OMHClientHelper {
                 synchronized(this) {
                     val authClient = getGoogleAuthClient(context)
 
-                    val authClientMigrator: AuthClientMigrator = AuthClientMigrator(authClient)
-
                     storageClientInstance =
                         OmhStorageProvider.Builder()
                             .addGmsPath("com.omh.android.storage.api.drive.gms.OmhGmsStorageFactoryImpl")
                             .addNonGmsPath("com.omh.android.storage.api.drive.nongms.OmhGmsStorageFactoryImpl")
                             .build()
-                            .provideStorageClient(authClientMigrator, context)
+                            .provideStorageClient(authClient, context)
                 }
             }
 
             return storageClientInstance!!
-        }
-    }
-}
-
-class AuthClientMigrator(val omhAuthClient: OmhAuthClient) : com.omh.android.auth.api.OmhAuthClient {
-    private lateinit var user: com.openmobilehub.android.auth.core.models.OmhUserProfile
-
-    init {
-        omhAuthClient.getUser().addOnSuccess {
-            this.user = it
-        }
-    }
-
-    override fun getAccountFromIntent(data: Intent?): OmhUserProfile {
-        return OmhUserProfile(user.name, user.surname, user.email, user.profileImage)
-    }
-
-    override fun getCredentials(): Any {
-        return omhAuthClient.getCredentials()
-    }
-
-    override fun getLoginIntent(): Intent {
-        return omhAuthClient.getLoginIntent()
-    }
-
-    override fun getUser(): OmhUserProfile {
-        return OmhUserProfile(user.name, user.surname, user.email, user.profileImage)
-    }
-
-    override fun revokeToken(): OmhTask<Unit> {
-        return OmhTaskMigrator(omhAuthClient.revokeToken())
-    }
-
-    override fun signOut(): OmhTask<Unit> {
-        return OmhTaskMigrator(omhAuthClient.signOut())
-    }
-
-    class OmhTaskMigrator<Unit>(private val omhTask: com.openmobilehub.android.auth.core.async.IOmhTask<kotlin.Unit>) :
-        OmhTask<Unit>() {
-        override fun execute(): OmhCancellableMigrator {
-            return OmhCancellableMigrator(omhTask.execute())
-        }
-
-        class OmhCancellableMigrator(private val omhCancellable: com.openmobilehub.android.auth.core.async.OmhCancellable) :
-            OmhCancellable {
-            override fun cancel() {
-                omhCancellable.cancel()
-            }
         }
     }
 }
