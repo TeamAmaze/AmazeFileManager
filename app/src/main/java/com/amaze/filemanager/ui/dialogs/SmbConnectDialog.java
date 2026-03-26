b3358aed89424f4f4f8b2744f95dec0726843ee2
Abdul Rauf <41958538+abdulroufsidhu@users.noreply.github.com>
2026-03-26 14:14:45 +0500

    Fix logical operator in SMB connection check

--- app/src/main/java/com/amaze/filemanager/ui/dialogs/SmbConnectDialog.java
+++ app/src/main/java/com/amaze/filemanager/ui/dialogs/SmbConnectDialog.java
@@ -347,7 +347,7 @@ public class SmbConnectDialog extends DialogFragment {
           SmbFile smbFile;
           String domaind = domain.getText().toString();
           if (chkSmbAnonymous.isChecked()
-              || (TextUtils.isEmpty(user.getText()) && TextUtils.isEmpty(pass.getText() )))
+              || (TextUtils.isEmpty(user.getText()) && TextUtils.isEmpty(pass.getText())))
             smbFile = createSMBPath(new String[] {ipa, "", "", domaind, sShare}, true, false);
           else {
             String useraw = user.getText().toString();
